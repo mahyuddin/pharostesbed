ea8a0c0cbfe2f8d1f5695062112b528861b5516b
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-02-05 02:19:23 +0000

    Initial Commit.

--- trunk/PharosMiddleware/src/playerclient/OpaqueInterface.java
+++ trunk/PharosMiddleware/src/playerclient/OpaqueInterface.java
@@ -1,7 +1,9 @@
 package playerclient;
 
-import java.util.*;
+import java.io.IOException;
 import playerclient.structures.PlayerMsgHdr;
+import playerclient.xdr.OncRpcException;
+import playerclient.xdr.XdrBufferDecodingStream;
 
 /**
  * The OpaqueInterface is used to exchange custom data between the PlayerServer
@@ -16,8 +18,46 @@ public class OpaqueInterface extends PlayerDevice {
 		super(pc); 
 	}
 	
-    protected void handleResponse (PlayerMsgHdr header) {
-        
-    }
+	public synchronized void readData (PlayerMsgHdr header) { 
+		System.out.println("OpaqueInterface: readData called, header:\n" + header.toString());
+		
+		if (header.getSubtype() == PLAYER_OPAQUE_DATA_STATE) {
+			int dataCount = 0;
+			byte[] data = null;
+			
+			// Read data_count
+			try {
+				byte[] buffer = new byte[header.getSize()];
+				is.readFully (buffer, 0, buffer.length);
+				
+				System.out.print("Bytes received: [" + buffer[0]);
+				for (int i=1; i < buffer.length; i++) {
+					System.out.print(", " + buffer[i]);
+				}
+				System.out.println("]");
+				
+				
+				XdrBufferDecodingStream xdr = new XdrBufferDecodingStream (buffer);
+				xdr.beginDecoding ();
+				dataCount = xdr.xdrDecodeInt();
+				
+				int arraySize = xdr.xdrDecodeInt();
+				data = xdr.xdrDecodeOpaque(arraySize);
+				xdr.endDecoding();
+				xdr.close ();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+			System.out.println("dataCount = " + dataCount);
+			System.out.print("Data: [" + data[0]);
+			for (int i=1; i < data.length; i++) {
+				System.out.print(", " + data[i]);
+			}
+			System.out.println("]");
+			String s = new String(data);
+			System.out.println("String: \"" + s + "\"");			
+		}
+	}
 	
 }
