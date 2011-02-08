18d57e10f2dbeda376a0c9901870eddee7b95951
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-02-08 20:11:38 +0000

    Created private branch for optimal movement senior design group.

---
+++ branches/optimalMovement/PharosMiddleware/src/pharoslabut/io/GPSMotionScriptMsg.java
@@ -0,0 +1,23 @@
+package pharoslabut.io;
+
+import pharoslabut.navigate.*;
+
+public class GPSMotionScriptMsg implements Message {
+	
+	private static final long serialVersionUID = -7631305555004386678L;
+	
+	private GPSMotionScript script;
+	
+	public GPSMotionScriptMsg(GPSMotionScript script) {
+		this.script = script;
+	}
+	
+	public GPSMotionScript getScript() {
+		return script;
+	}
+	
+	@Override
+	public MsgType getType() {
+		return MsgType.LOAD_GPS_MOTION_SCRIPT;
+	}
+}
