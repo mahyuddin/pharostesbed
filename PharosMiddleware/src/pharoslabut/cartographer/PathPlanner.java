d5557981d0f84ed56eafc4c0f8ef0cf2b62db74d
kevinaboos <kevinaboos@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-02-09 23:45:31 +0000

    added motion tests and odometry reading, still needs accuracy tests

--- PharosMiddleware/src/pharoslabut/cartographer/PathPlanner.java
+++ PharosMiddleware/src/pharoslabut/cartographer/PathPlanner.java
@@ -4,13 +4,15 @@ import pharoslabut.MotionArbiter;
 import pharoslabut.logger.FileLogger;
 import pharoslabut.tasks.MotionTask;
 import pharoslabut.tasks.Priority;
-import pharoslabut.validation.MotorStressTest;
 import playerclient.PlayerClient;
 import playerclient.PlayerException;
 import playerclient.Position2DInterface;
+import playerclient.Position2DListener;
 import playerclient.structures.PlayerConstants;
+import playerclient.structures.PlayerPose;
+import playerclient.structures.position2d.PlayerPosition2dData;
 
-public class PathPlanner {
+public class PathPlanner implements Position2DListener {
 	private PlayerClient client = null;
 	private FileLogger flogger = null;
 	
@@ -23,11 +25,15 @@ public class PathPlanner {
 			System.exit (1);
 		}
 		
+		//client.requestInterfacePosition2D(0, PlayerConstants.PLAYER_OPEN_MODE);
+		
 		Position2DInterface motors = client.requestInterfacePosition2D(0, PlayerConstants.PLAYER_OPEN_MODE);
 		if (motors == null) {
-			log("motors is null");
+			log("unable to connect to Position2D interface");
 			System.exit(1);
 		}
+		
+		motors.addPos2DListener(this); 		
 		
 		MotionArbiter motionArbiter = null;
 		
@@ -39,32 +45,31 @@ public class PathPlanner {
 		}
 		
 		MotionTask currTask;
-		double speedStep = .3;
+		double speedStep = .2;
 		
 		pause(2000);
 		currTask = new MotionTask(Priority.SECOND, speedStep, MotionTask.STOP_HEADING);
 		log("Submitting: " + currTask);
 		motionArbiter.submitTask(currTask);
-		pause(3000);
-		
-		currTask = new MotionTask(Priority.FIRST, MotionTask.STOP_VELOCITY, MotionTask.STOP_HEADING);
-		log("Submitting: " + currTask);
-		motionArbiter.submitTask(currTask);
 		pause(5000);
 		
-		/*currTask = new MotionTask(Priority.SECOND, -speedStep, 0);
-		log("Submitting: " + currTask);
-		motionArbiter.submitTask(currTask);
-		pause(3000);
-		
-		// Stop the robot
 		currTask = new MotionTask(Priority.FIRST, MotionTask.STOP_VELOCITY, MotionTask.STOP_HEADING);
 		log("Submitting: " + currTask);
-		motionArbiter.submitTask(currTask);*/
+		motionArbiter.submitTask(currTask);
+		pause(1000);
+		
 		
 		log("Test complete!");
 		System.exit(0);
 	}
+	
+
+	@Override
+	public void newPlayerPosition2dData(PlayerPosition2dData data) {
+		PlayerPose pp = data.getPos();
+		log("Odometry Data: x=" + pp.getPx() + ", y=" + pp.getPy() + ", a=" + pp.getPa() + ", vel=" + data.getVel() + ", stall=" + data.getStall());
+	}
+	
 	
 	private void pause(int duration) {
 		synchronized(this) {
@@ -77,7 +82,7 @@ public class PathPlanner {
 	}
 	
 	private void log(String msg) {
-		String result = "MotorStressTest: " + msg;
+		String result = "PathPlanner: " + msg;
 		System.out.println(result);
 		if (flogger != null) {
 			flogger.log(result);
@@ -85,7 +90,7 @@ public class PathPlanner {
 	}
 	
 	private static void usage() {
-		System.err.println("Usage: pharoslabut.validation.MotorStressTest <options>\n");
+		System.err.println("Usage: pharoslabut.validation.PathPlanner <options>\n");
 		System.err.println("Where <options> include:");
 		System.err.println("\t-server <ip address>: The IP address of the Player Server (default localhost)");
 		System.err.println("\t-port <port number>: The Player Server's port number (default 6665)");
@@ -93,6 +98,11 @@ public class PathPlanner {
 		System.err.println("\t-car: issue car like commands (default non-car-like)");
 	}
 	
+	
+	/******************** MAIN ****************************
+	 * runs the whole shit
+	 * @param args
+	 *****************************************************/
 	public static void main(String[] args) {
 		String fileName = "log.txt";
 		String serverIP = "10.11.12.10"; // server for St. Arnold
@@ -125,7 +135,13 @@ public class PathPlanner {
 		System.out.println("File: " + fileName);
 		
 		new PathPlanner(serverIP, serverPort, fileName);
+		
+		
+		
 	}
+
+		
 }
+
 
 
