3bb99bda04a93ae2a2b5d64bdfcb3fb92d7c085b
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-02-15 14:46:05 +0000

    Initial commit.

---
+++ PharosMiddleware/src/pharoslabut/RobotIPAssignments.java
@@ -0,0 +1,30 @@
+package pharoslabut;
+
+/**
+ * Defines the last octal of each robot's IP address.
+ * 
+ * @author Chien-Liang Fok
+ *
+ */
+public interface RobotIPAssignments {
+	public static final int ADAMS = 16;
+	public static final int ADVENTINUS = 28;
+	public static final int BIGFOOT = 36;
+	public static final int CHIMAY = 23;
+	public static final int CZECHVAR = 14;
+	public static final int FRAMBOISE = 31;
+	public static final int GUINNESS = 20;
+	public static final int HARP = 30;
+	public static final int KONA = 27;
+	public static final int LIVEOAK = 26;
+	public static final int MANNY = 13;
+	public static final int MARDESOUS = 24;
+	public static final int NEGRAMONDELO = 34;
+	public static final int NEWCASTLE = 29;
+	public static final int PYRAMID = 33;
+	public static final int SHINER = 17;
+	public static final int SPATEN = 35;
+	public static final int WYNKOOP = 25;
+	public static final int XUEHUA = 32;
+	public static final int ZIEGEN = 18;
+}
