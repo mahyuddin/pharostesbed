16b22e7ae2c97d5e9cde327c772d3a471c3e87fc
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-02-16 04:20:53 +0000

    Initial Commit.

---
+++ PharosMiddleware/src/pharoslabut/logger/analyzer/SignalStrengthVsDist.java
@@ -0,0 +1,14 @@
+package pharoslabut.logger.analyzer;
+
+/**
+ * This analyzes the log files from and experiment and extracts the signal
+ * strength and distance data.
+ * 
+ * @author Chien-Liang Fok
+ */
+public class SignalStrengthVsDist {
+
+	public SignalStrengthVsDist() {
+		
+	}
+}
