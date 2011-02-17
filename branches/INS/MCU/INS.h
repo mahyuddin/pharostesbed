5e694982c7435a9e058e3e0662c96b15ac371cfa
reilaos <reilaos@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-02-17 00:38:11 +0000

    Change to use multiple axes.

--- branches/INS/MCU/INS.h
+++ branches/INS/MCU/INS.h
@@ -2,7 +2,7 @@
 #include "adc.c"
 
 
-#define INS_SAMPLE_FREQ 200
+#define INS_SAMPLE_FREQ 3000
 
 // Making a table from 10-bit ADC to M/S^2
 // Accel is in signed, fixed point, where 1 = 0.001 M/S^2
