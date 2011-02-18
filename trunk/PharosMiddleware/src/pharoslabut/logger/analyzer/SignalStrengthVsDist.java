5611365286e57ddd8f03b4c1e8fbc01ca75ec81e
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-02-18 20:17:27 +0000

    Added expData variable.

--- trunk/PharosMiddleware/src/pharoslabut/logger/analyzer/SignalStrengthVsDist.java
+++ trunk/PharosMiddleware/src/pharoslabut/logger/analyzer/SignalStrengthVsDist.java
@@ -4,11 +4,19 @@ package pharoslabut.logger.analyzer;
  * This analyzes the log files from and experiment and extracts the signal
  * strength and distance data.
  * 
+ * It produces a log file with the following format
+ * 
  * @author Chien-Liang Fok
  */
 public class SignalStrengthVsDist {
-
-	public SignalStrengthVsDist() {
+	
+	ExpData expData;
+	
+	public SignalStrengthVsDist(String expDir) {
+		expData = new ExpData(expDir);
+	}
+	
+	public void analyzeTelosBSignal() {
 		
 	}
 }
