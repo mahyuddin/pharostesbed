ac0df20ec0166f601cb5c4493eee88c201ef0308
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-02-20 05:21:54 +0000

    Added constants.

--- branches/demoPeaseElementary/PharosMiddleware/src/pharoslabut/demo/irobotcam/MCUConstants.java
+++ branches/demoPeaseElementary/PharosMiddleware/src/pharoslabut/demo/irobotcam/MCUConstants.java
@@ -1,11 +1,23 @@
 package pharoslabut.demo.irobotcam;
 
 public interface MCUConstants {
+	
+	/**
+	 * The amount of time in milliseconds to wait for an ack from the MCU.
+	 */
+	public static final int MCU_TIMEOUT_PERIOD = 3000;
+	
 	public static final byte PROTEUS_BEGIN = 0x24;
 	public static final byte PROTEUS_END = 0x0A;
 	public static final byte PROTEUS_ESCAPE = (byte)0xFF;
 	
 	public static final byte PROTEUS_OPCODE_CAMERA_PAN = 0x6C;
 	public static final byte PROTEUS_OPCODE_CAMERA_TILT = 0x6D;
-
+	
+	public static final byte PROTEUS_STATUS_PACKET = 7;
+	public static final byte PROTEUS_TEXT_MESSAGE_PACKET = 9;
+	public static final byte PROTEUS_ACK_PACKET = 10;
+	
+	public static final int MCU_STATUS_PACKET_SIZE = 7;
+	public static final int MCU_ACK_PACKET_SIZE = 3;
 }
