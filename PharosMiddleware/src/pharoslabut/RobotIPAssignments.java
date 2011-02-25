4b3c30c2bd35a51c51b9ca5374efb0cf1ba6b176
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-02-25 22:58:46 +0000

    Initial commit.

--- PharosMiddleware/src/pharoslabut/RobotIPAssignments.java
+++ PharosMiddleware/src/pharoslabut/RobotIPAssignments.java
@@ -1,12 +1,14 @@
 package pharoslabut;
 
+import java.lang.reflect.Field;
+
 /**
  * Defines the last octal of each robot's IP address.
  * 
  * @author Chien-Liang Fok
  *
  */
-public interface RobotIPAssignments {
+public class RobotIPAssignments {
 	public static final int ADAMS = 16;
 	public static final int ADVENTINUS = 28;
 	public static final int BIGFOOT = 36;
@@ -28,4 +30,77 @@ public interface RobotIPAssignments {
 	public static final int WYNKOOP = 25;
 	public static final int XUEHUA = 32;
 	public static final int ZIEGEN = 18;
+	
+	
+	/**
+	 * Returns the robot's ID, which is the last octal of the robot's IP address.
+	 * 
+	 * @param name The name of the robot
+	 * @return the last octal of the robot's IP address, or -1 if IP address is unknown.
+	 */
+	public static int getRobotID(String name) {
+		Field[] fields;
+		try {
+			fields = Class.forName("pharoslabut.RobotIPAssignments").getDeclaredFields();
+			
+			// For each field in the robot IP assignment table...
+			for (int i = 0; i < fields.length; i++) {
+				Field currField = fields[i];
+				String currFieldName = currField.getName().toLowerCase();
+				
+				// If the name of the log file contains the name of the robot...
+				if (name.toLowerCase().equals(currFieldName)) {
+					
+					// Find and return the last octal of the IP address...
+					return currField.getInt(null);
+				}
+			}
+				
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		
+		System.err.println("ERROR: Unable to find IP for robot " + name);
+		return -1;
+	}
+	
+	/**
+	 * Returns the robot's name.
+	 * 
+	 * @param id The id of the robot, which is the last octal of the robot's IP address
+	 * @return the name of the robot, or null if unknown.
+	 */
+	public static String getRobotName(int id) {
+		Field[] fields;
+		try {
+			fields = Class.forName("pharoslabut.RobotIPAssignments").getDeclaredFields();
+			
+			// For each field in the robot IP assignment table...
+			for (int i = 0; i < fields.length; i++) {
+				Field currField = fields[i];
+				if (id == currField.getInt(null)) {
+					return currField.getName();
+				}
+			}
+				
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		
+		System.err.println("ERROR: Unable to find name of robot with ID " + id);
+		return null;
+	}
+	
 }
