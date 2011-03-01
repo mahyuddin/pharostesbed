c9bb6c128ed119dccc9960dcc78ed33adcd72504
kevinaboos <kevinaboos@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-03-01 05:57:05 +0000

    added updateLocation method, the math needs to be checked. added calibration methods, need to be filled in with appropriate math functions once calibration data is complete.

--- branches/cartographer/PharosMiddleware/src/pharoslabut/cartographer/LocationTracker.java
+++ branches/cartographer/PharosMiddleware/src/pharoslabut/cartographer/LocationTracker.java
@@ -1,28 +1,78 @@
 package pharoslabut.cartographer;
 
+import playerclient.structures.PlayerPose;
+
+
 public class LocationTracker {
-	private static int currentX;
-	private static int currentY;
 	
-	private static double bearing; // 0 to 2pi radians counterclockwise
-	  // 0 is North, pi/2 is West, pi is South, 3*pi/2 (-pi/2) is East
+	// change initial values to reflect the robot's starting orientation
+	private static final double initialBearing = 0;
+	private static final double initialX = 0;
+	private static final double initialY = 0;
+	
+	private static double currentX;
+	private static double currentY;
+	
+	private static double bearing; // -PI to +PI
+	// 0 is north, -PI/2 is west, -PI or +PI are both south, and PI/2 is east
+	
 	
 	public LocationTracker() {
-		// robot begins in upper-left corner facing down (south)
-		currentX = 0; currentY = 0; bearing = Math.PI; 
+		// robot begins in upper-left corner with a bearing of "initialBearing"
+		currentX = initialX; currentY = initialY; bearing = initialBearing; 
 		
 		// To reset the robot's odometry to (x, y, yaw) = (0,0,0), send
 		// a PLAYER_POSITION2D_REQ_RESET_ODOM request.  Null response.
 		// note: the PlayerClient pkgs refer to the turn angle as "yaw"
-				
-		
+		PathPlanner.writeOdometry(initialX, initialY, initialBearing);		
 		
 	}
 	
-	public static synchronized void updateLocation() { 
-		// this might take in parameters, or it could read them in here
-		// remember, it must factor in expected location from PathPlanner, 
-		   // and also real location from Player odometer
+	
+	public static void updateLocation(PlayerPose newLoc) { 
 		
+		currentX = calibrateX(newLoc.getPx());
+		currentY = calibrateY(newLoc.getPy());
+		
+		// if the writeOdometry line in the Constructor works, then we don't need to add initialBearing to the calibrated new location
+		bearing = (Math.abs(initialBearing + calibrateAngle(newLoc.getPa()))); 
+		if (bearing < -Math.PI) { 
+			bearing = (bearing % (2*Math.PI)) + Math.PI;
+		} else if (bearing > Math.PI) {
+			bearing = (bearing % (2*Math.PI)) - Math.PI;
+		}
+		
+		WorldView.recordLocation(currentX, currentY);
+	}
+	
+	
+	/**
+	 * @author Kevin
+	 * @return array of doubles: {currentX, currentY, bearing}
+	 */
+	public static double [] getCurrentLocation () {
+		double [] loc = {currentX, currentY, bearing};
+		return loc;
+	}
+	
+	
+	private static double calibrateX(double xValue) {
+		// take odometer value and convert to accurate X distance
+		return xValue;
+	}
+	
+	
+	private static double calibrateY(double yValue) {
+		// take odometer value and convert to accurate Y distance
+		return yValue;
+	}
+	
+	
+	private static double calibrateAngle(double angle) {
+		// take odometer value and convert to accurate angle measurement
+		return angle;
 	}
 }
+
+
+
