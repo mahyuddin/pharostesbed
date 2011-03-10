0f5d26876aae135383679159091389ced3f25c3b
michaeljean <michaeljean@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-03-10 22:14:04 +0000

    Committing the AIM project from netbeans

--- branches/autoIntersection/AIM/src/aim/Main.java
+++ branches/autoIntersection/AIM/src/aim/Main.java
@@ -9,11 +9,15 @@ import java.util.Date;
  */
 public class Main {
 
+    /**
+     * startTime is a constant containing the time when the run started
+     */
     public static final long startTime = new Date().getTime();
 
     /**
      * call the IntersectionManager and start running the code
      * @param args the command line arguments
+     * @throws InterruptedException
      */
     public static void main(String[] args) throws InterruptedException  {
         RobotsPriorityQueue.test();
