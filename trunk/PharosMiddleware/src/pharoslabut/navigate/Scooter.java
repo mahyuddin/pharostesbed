63a7bdcd4a4d890ee86c63bbf37c12c990082d3f
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-03-26 12:47:48 +0000

    Modified to continuously scoot until forced to stop by a second instruction.

--- trunk/PharosMiddleware/src/pharoslabut/navigate/Scooter.java
+++ trunk/PharosMiddleware/src/pharoslabut/navigate/Scooter.java
@@ -11,10 +11,11 @@ import pharoslabut.logger.FileLogger;
  * 
  * @author Chien-Liang Fok
  */
-public class Scooter {
+public class Scooter implements Runnable {
 
 	private MotionArbiter motionArbiter;
 	private FileLogger flogger;
+	private boolean running = false;
 	
 	public Scooter(MotionArbiter motionArbiter, FileLogger flogger) {
 		this.motionArbiter = motionArbiter;
@@ -22,21 +23,35 @@ public class Scooter {
 	}
 	
 	public void scoot(int amt) {
-		log("Scooting robot " + amt + " units");
+		running = true;
+		new Thread(this).start();
+	}
+	
+	public void run() {
+		log("Scooting robot..");
 		
 		MotionTask currTask;
-		double speed = 0.5;
-		if (amt < 0)
-			speed *= -1;
+		boolean goForward = true;
 		
-		currTask = new MotionTask(Priority.SECOND, speed, 0); // move forward or backwards
-		log("Submitting: " + currTask);
-		motionArbiter.submitTask(currTask);
+		while (running) {
 		
-		pause(1000*amt);
-		currTask = new MotionTask(Priority.SECOND, MotionTask.STOP_VELOCITY, MotionTask.STOP_HEADING);
-		log("Submitting: " + currTask);
-		motionArbiter.submitTask(currTask);
+			double speed = 1;
+			if (!goForward)
+				speed = -1.05;
+			goForward = !goForward;
+		
+			currTask = new MotionTask(Priority.SECOND, speed, 0); // move forward or backwards
+			log("Submitting: " + currTask);
+			motionArbiter.submitTask(currTask);
+		
+			pause(1500);
+			
+			currTask = new MotionTask(Priority.SECOND, MotionTask.STOP_VELOCITY, MotionTask.STOP_HEADING);
+			log("Submitting: " + currTask);
+			motionArbiter.submitTask(currTask);
+			
+			pause(1000);
+		}
 	}
 	
 	private void pause(int duration) {
