63170bbe1329481abde052c31b166951f2a4b3a5
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-03-26 12:49:13 +0000

    Modified to provide access to beacon receiver.  Needed by new RCV_TELOSB_BEACON instruction used in Mission 16.

--- PharosMiddleware/src/pharoslabut/radioMeter/cc2420/TelosBeaconReceiver.java
+++ PharosMiddleware/src/pharoslabut/radioMeter/cc2420/TelosBeaconReceiver.java
@@ -12,21 +12,62 @@ public class TelosBeaconReceiver implements MessageListener{
 //	private MoteIF moteIF;
 	private FileLogger flogger;
 	
+	int currCount;
+	int targetCount;
+	
+	/**
+	 * The ID of the mote that is attached to this device.  This ID is included
+	 * in the beacon to identify the transmitter.
+	 */
+	private int moteID;
+	
 	public TelosBeaconReceiver(MoteIF moteIF) {
+		try {
+			// Get the local node ID
+			moteID = TelosBeaconBroadcaster.getMoteID();
+			log("Mote ID = " + moteID);
+		} catch(TelosBeaconException e) {
+			log("Unable to get moteID, assuming it is zero");
+			moteID = 0;
+		}
+		
 //		this.moteIF = moteIF;
 		moteIF.registerListener(new RadioSignalResultsMsg(), this);
+	}
+	
+	public void rcvBeacons(int targetCount) {
+		log("Waiting until " + targetCount + " beacons are received...");
+		this.currCount = 0;
+		this.targetCount = targetCount;
+		
+		while (currCount < targetCount) {
+			synchronized(this) {
+				try {
+					this.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			log("Received " + currCount + " of " + targetCount + " beacons");
+		}
 	}
 	
 	public void messageReceived(int to, Message message) {
 		if (message instanceof RadioSignalResultsMsg) {
 			RadioSignalResultsMsg resultMsg = (RadioSignalResultsMsg)message;
 			log("RADIO_CC2420_RECEIVE" 
-				+ "\t" + resultMsg.get_idReceiver()
+				+ "\t" + moteID //resultMsg.get_idReceiver()
 				+ "\t" + resultMsg.get_idSender()
 				+ "\t" + resultMsg.get_seqno()
 				+ "\t" + resultMsg.get_rssi()
 				+ "\t" + resultMsg.get_lqi()
 				+ "\t" + resultMsg.get_timestamp());
+			
+			// Update the number of beacons received.
+			currCount++;
+			synchronized(this) {
+				notifyAll();
+			}
 		}
 		else {
 			log("UNKNOWN_MSG"
