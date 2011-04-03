789b5e03e1542005624ce2ca003f7c1e8454b3d9
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-04-03 05:45:33 +0000

    Added javadoc.

--- branches/demoPeaseElementary/PharosMiddleware/src/pharoslabut/demo/irobotcam/PlayerControlCmd.java
+++ branches/demoPeaseElementary/PharosMiddleware/src/pharoslabut/demo/irobotcam/PlayerControlCmd.java
@@ -1,5 +1,11 @@
 package pharoslabut.demo.irobotcam;
 
+/**
+ * Defines the commands that can be sent to the player server.
+ * 
+ * @author Chien-Liang Fok
+ *
+ */
 public enum PlayerControlCmd {
 	START, STOP;
 }
