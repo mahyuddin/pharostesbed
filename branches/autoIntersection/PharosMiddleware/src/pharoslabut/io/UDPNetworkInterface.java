e4dabf38937bebe56dbe629c5dda2fc1baecd525
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-04-20 21:02:18 +0000

    Initial commit.  Tested on two computers.

---
+++ branches/autoIntersection/PharosMiddleware/src/pharoslabut/io/UDPNetworkInterface.java
@@ -0,0 +1,155 @@
+package pharoslabut.io;
+
+import java.io.*;
+import java.net.*;
+
+/**
+ * The UDP Network Interface uses UDP to transmit and receive messages.
+ * 
+ * @author Chien-Liang Fok
+ */
+public class UDPNetworkInterface extends NetworkInterface {
+
+	private DatagramSocket socket;
+	private int port;
+    private UDPReceiver rcvr;
+    
+    /**
+     * The constructor.
+     * 
+     * @param port The port on which to listen for incoming messages.
+     */
+	public UDPNetworkInterface(int port) {
+		this.port = port;
+		openSocket();
+		rcvr = new UDPReceiver();
+	}
+	
+	@Override
+	public void stop() {
+		rcvr.stop();
+	}
+	
+	@Override
+	public boolean sendMessage(InetAddress address, int port, Message m) {
+		
+        if(m == null) {
+        	log("ERROR: Attempted to send null message...", false);
+        	return false;
+        }
+        
+        if (socket == null) {
+        	log("ERROR: while trying to send, the socket is null...", false);
+        	return false;
+        }
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        
+        try {
+        	ObjectOutputStream oos = new ObjectOutputStream(bos);
+        	oos.writeObject(m);
+        	oos.flush();
+        	oos.close();
+        	
+        	byte[] sendByte = bos.toByteArray();
+        	bos.close();
+        	
+        	DatagramPacket pkt = new DatagramPacket(sendByte, sendByte.length, address, port);
+        	socket.send(pkt);
+        	
+        	log("Sent message " + m + " to " + address + ":" + port);
+        } catch(IOException ioe) {
+        	log("ERROR: while sending datagram packet, error: " + ioe.getMessage(), false);
+        	ioe.printStackTrace();
+        	return false;
+        }
+        
+        return true;
+	}
+	
+	/**
+	 * Opens the server socket.  If an error occurs while opening the socket,
+	 * it waits two seconds before trying again.
+	 */
+	private void openSocket() {
+		while (socket == null) {
+			log("Opening server socket...");
+			try {
+				socket = new DatagramSocket(port);
+			} catch (SocketException e) {
+				log("ERROR: Failed to open server socket, error: " + e.getMessage(), false);
+
+				try {
+					synchronized(this) {
+						this.wait(2000); // pause 2 seconds
+					}
+				} catch(InterruptedException ie) {
+					ie.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	
+	/**
+	 * Listens for incoming messages.  Executes using its own thread.
+	 */
+	private class UDPReceiver implements Runnable {
+		boolean running = true;
+		
+		public UDPReceiver() {
+			new Thread(this).start();
+		}
+		
+		/**
+		 * Stops this UDPReceiver.
+		 */
+		public void stop() {
+			running = false;
+		}
+		
+		public void run() {
+			
+			while(running) {
+				if (socket == null) 
+					openSocket();
+				if (socket != null) {
+					try {
+						byte[] receiveByte = new byte[256];
+						DatagramPacket rpkt = new DatagramPacket(receiveByte, receiveByte.length);
+						socket.receive(rpkt);
+						
+						//extract data from received packet and print it
+						ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(rpkt.getData()));
+			            try {
+							Message m = (Message)ois.readObject();
+							newMessage(m); // notify all listeners of message
+						} catch (ClassNotFoundException e) {
+							log("ERROR: while extracting object from datagram packet: " + e.getMessage(), false);
+							e.printStackTrace();
+						}
+			            
+					} catch(IOException ioe) {
+						log("ERROR: while receiving packet: " + ioe.getMessage(), false);
+						ioe.printStackTrace();
+						socket = null;
+					}
+				}
+			}
+			
+			socket.close();
+		}
+	}
+	
+	private void log(String msg) {
+		log(msg, true);
+	}
+	
+	private void log(String msg, boolean isDebugMsg) {
+		String result = "UDPNetworkInterface: " + msg;
+		if (System.getProperty ("PharosMiddleware.debug") != null || !isDebugMsg)
+			System.out.println(result);
+		if (flogger != null)
+			flogger.log(result);
+	}
+}
---
+++ branches/autoIntersection/PharosMiddleware/src/pharoslabut/tests/TestNetworkInterface.java
@@ -0,0 +1,126 @@
+package pharoslabut.tests;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import pharoslabut.ExpType;
+import pharoslabut.io.*;
+import pharoslabut.logger.*;
+
+/**
+ * Tests the network interface abstractions.
+ * 
+ * @author Chien-Liang Fok
+ *
+ */
+public class TestNetworkInterface implements MessageReceiver{
+	
+	private FileLogger flogger;
+	private NetworkInterface ni;
+	
+	public TestNetworkInterface(boolean useUDP, String serverIP, int serverPort, FileLogger flogger) {
+		this.flogger = flogger;
+		
+		if (useUDP) {
+			ni = new UDPNetworkInterface(serverPort);
+		} else {
+			log("TCP testing not implemented yet.");
+		}
+		
+		ni.registerMsgListener(this);
+		
+		InetAddress address = null;
+		try {
+			address = InetAddress.getByName(serverIP);
+		} catch (UnknownHostException e) {
+			log("ERROR: Unable to get server address...");
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		int cntr = 0;
+		
+		while (true) {
+			Message m = new StartExpMsg("TestExp", "Msg-" + cntr, ExpType.FOLLOW_GPS_MOTION_SCRIPT);
+			
+			
+			if (ni.sendMessage(address, serverPort, m)) {
+				log("Sent message " + cntr);
+				cntr++;
+			} else {
+				log("Failed to send message, waiting an then trying again...");
+			}
+			
+			try {
+				synchronized(this) {
+					wait(1000);
+				}
+			} catch(InterruptedException ie) {
+				ie.printStackTrace();
+			}
+		}
+	}
+	
+	@Override
+	public void newMessage(Message msg) {
+		log("Received message: " + msg);
+	}
+	
+	private void log(String msg) {
+		String result = "TestNetworkInterface: " + msg;
+		System.out.println(result);
+		if (flogger != null)
+			flogger.log(result);
+	}
+	
+	private static void usage() {
+		System.err.println("Usage: pharoslabut.tests.TestNetworkInterface <options>\n");
+		System.err.println("Where <options> include:");
+		System.err.println("\t-protocol <type>: The protocol type, either TCP or UDP (default UDP)");
+		System.err.println("\t-server <ip address>: The IP address of the remote host (default localhost)");
+		System.err.println("\t-port <port number>: The port on which the remote host is listening (default 8889)");
+		System.err.println("\t-file <file name>: name of file in which to save results (default null)");
+		System.err.println("\t-debug: enable debug mode");
+	}
+	
+	public static void main(String[] args) {
+		FileLogger flogger = null;
+		String serverIP = "localhost";
+		boolean useUDP = true;
+		int serverPort = 8889;
+
+		try {
+			for (int i=0; i < args.length; i++) {
+				if (args[i].equals("-protocol")) {
+					useUDP = !args[++i].toUpperCase().equals("TCP");
+				}
+				if (args[i].equals("-server")) {
+					serverIP = args[++i];
+				} 
+				else if (args[i].equals("-port")) {
+					serverPort = Integer.valueOf(args[++i]);
+				} 
+				else if (args[i].equals("-file")) {
+					flogger = new FileLogger(args[++i]);
+				}
+				else if (args[i].equals("-debug") || args[i].equals("-d")) {
+					System.setProperty ("PharosMiddleware.debug", "true");
+				}
+				else {
+					usage();
+					System.exit(1);
+				}
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+			usage();
+			System.exit(1);
+		}
+		
+//		log("Protocol: " + (useUDP ? "UDP" : "TCP"));
+//		log("Server IP: " + serverIP);
+//		log("Server port: " + serverPort);
+		
+		new TestNetworkInterface(useUDP, serverIP, serverPort, flogger);
+	}
+}
