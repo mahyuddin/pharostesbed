d4fd4a652295e5bb0ca4e352223b7e0bb21371b5
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-04-21 21:29:40 +0000

    Initial commit.

---
+++ branches/autoIntersection/PharosMiddleware/src/pharoslabut/demo/autoIntersection/LocalIntersectionManager.java
@@ -0,0 +1,11 @@
+package pharoslabut.demo.autoIntersection;
+
+/**
+ * Handles the protocol for navigating through an intersection
+ * without a server.
+ * 
+ * @author Seth Gee
+ */
+public class LocalIntersectionManager {
+
+}
