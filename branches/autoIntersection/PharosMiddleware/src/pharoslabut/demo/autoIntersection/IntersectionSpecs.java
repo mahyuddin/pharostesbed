c964392c8865c4bec7683ca39f53894f93dfddcc
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-04-24 16:21:41 +0000

    Initial commit.

---
+++ branches/autoIntersection/PharosMiddleware/src/pharoslabut/demo/autoIntersection/IntersectionSpecs.java
@@ -0,0 +1,20 @@
+package pharoslabut.demo.autoIntersection;
+
+/**
+ * The intersection specifications.  Describes the intersection, i.e., how many roads are intersecting, how many lanes,
+ * whether the vehicles may turn, etc
+ * 
+ * @author Chien-Liang Fok
+ */
+public class IntersectionSpecs implements java.io.Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6517026202617597952L;
+
+	public IntersectionSpecs() {
+		
+	}
+
+}
---
+++ branches/autoIntersection/PharosMiddleware/src/pharoslabut/demo/autoIntersection/LaneSpecs.java
@@ -0,0 +1,23 @@
+package pharoslabut.demo.autoIntersection;
+
+/**
+ * The lane specifications.  Describes which lane the robot is traveling through.
+ * 
+ * @author Chien-Liang Fok
+ */
+public class LaneSpecs implements java.io.Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3753742070260486383L;
+
+	public LaneSpecs() {
+		
+	}
+	
+	public String toString() {
+		return "LaneSpecs";
+	}
+	
+}
