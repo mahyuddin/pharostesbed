d2ed30199d5f7363e62c1f4418ea3723543837a5
atxsasquatch <atxsasquatch@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-04-25 18:32:50 +0000

    Added IRListener class to begin work on IR sensors for local intersection manager.

---
+++ branches/autoIntersection/PharosMiddleware/src/playerclient/IRListener.java
@@ -0,0 +1,19 @@
+package playerclient;
+
+import playerclient.structures.ir.*;
+
+/**
+ * Defines the interface that all IR Listeners must implement.
+ * 
+ * @author Xiaomeng Wu
+ */
+public interface IRListener {
+
+	/**
+	 * This is called by IRInterface whenever a new IRData is
+	 * received.
+	 * 
+	 * @param data The new data received.
+	 */
+	public void newPlayerIRData(PlayerIrData data);
+}
