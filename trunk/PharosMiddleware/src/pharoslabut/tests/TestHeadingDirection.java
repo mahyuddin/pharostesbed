c43237e702df6172de69a41dd0c16d12c1f32dde
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-05-09 02:06:41 +0000

    Debugged.  Tested to work on Proteus with Traxxas.

--- trunk/PharosMiddleware/src/pharoslabut/tests/TestHeadingDirection.java
+++ trunk/PharosMiddleware/src/pharoslabut/tests/TestHeadingDirection.java
@@ -1,6 +1,7 @@
 package pharoslabut.tests;
 
 import pharoslabut.MotionArbiter;
+import pharoslabut.MotionArbiter.MotionType;
 import pharoslabut.tasks.MotionTask;
 import pharoslabut.tasks.Priority;
 import playerclient3.PlayerClient;
@@ -9,7 +10,7 @@ import playerclient3.Position2DInterface;
 import playerclient3.structures.PlayerConstants;
 
 /**
- * Tests the semantics of the heading in a MotionTask.  It shows that
+ * Tests the semantics of the heading in a MotionTask on the Traxxas mobility plane.  It shows that
  * a positive heading value means "turn left" and a negative heading means
  * "turn right".
  * 
@@ -19,10 +20,15 @@ import playerclient3.structures.PlayerConstants;
 public class TestHeadingDirection {
 
 	public static final void main(String[] args) {
-		String serverIP = "10.11.12.20";
+		String serverIP = "192.168.1.120";
 		int serverPort = 6665;
 
+		System.setProperty ("PlayerClient.debug", "true");
+		System.out.println("Launching TestHeadingDirection...");
+		
 		PlayerClient client = null;
+		
+		System.out.println("Connecting to the player server " + serverIP + ":" + serverPort + "...");
 		try {
 			client = new PlayerClient(serverIP, serverPort);
 		} catch(PlayerException e) {
@@ -30,38 +36,58 @@ public class TestHeadingDirection {
 			System.err.println("    [ " + e.toString() + " ]");
 			System.exit (1);
 		}
-
-		Position2DInterface motors = client.requestInterfacePosition2D(0, PlayerConstants.PLAYER_OPEN_MODE);
-
+		
+		// For some reason, we need to get the Position2DInterface *before* changing the player server mode to PUSH 
+		// or to make it run in threaded mode.  If we do not do this, the following method call will hang indefinately.
+		System.out.println("Subscribing to Position2DInterface...");
+		Position2DInterface motors = client.requestInterfacePosition2D(0 /* device index */, PlayerConstants.PLAYER_OPEN_MODE);
+		
 		if (motors == null) {
-			System.err.println("motors is null");
+			System.err.println("Motors is null");
 			System.exit(1);
 		}
+		
+		// The runThreaded and change of data delivery mode can be done in reverse order
+		// without breaking this test.
+		System.out.println("Setting Player Client to run in continuous threaded mode...");
+		client.runThreaded(-1, -1);
+		
+		System.out.println("Changing Player server mode to PUSH...");
+		client.requestDataDeliveryMode(playerclient3.structures.PlayerConstants.PLAYER_DATAMODE_PUSH);
 
-		MotionArbiter motionArbiter = new MotionArbiter(motors);
+		MotionTask mt;
+		
+		System.out.println("Creating MotionArbiter...");
+		MotionArbiter motionArbiter = new MotionArbiter(MotionType.MOTION_TRAXXAS, motors);
 
-		MotionTask mt1 = new MotionTask(Priority.SECOND, 0, 0.5); // turn left
-		motionArbiter.submitTask(mt1);
+		System.out.println("Turning the front wheels left...");
+		mt = new MotionTask(Priority.SECOND, 0, 0.5); // turn left
+		motionArbiter.submitTask(mt);
+		
 		try {
-			synchronized(mt1) {
-				mt1.wait(1000*5);
+			synchronized(mt) {
+				mt.wait(1000*5);
 			}
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		
-		MotionTask mt3 = new MotionTask(Priority.SECOND, 0, -0.5); // turn right
-		motionArbiter.submitTask(mt3);
+		System.out.println("Turning the front wheels right...");
+		mt = new MotionTask(Priority.SECOND, 0, -0.5); // turn right
+		motionArbiter.submitTask(mt);
 		try {
-			synchronized(mt3) {
-				mt3.wait(1000*5);
+			synchronized(mt) {
+				mt.wait(1000*5);
 			}
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		
-		MotionTask mt2 = new MotionTask(Priority.SECOND, MotionTask.STOP_SPEED, MotionTask.STOP_HEADING);
-		motionArbiter.submitTask(mt2);
+		System.out.println("Centering the front wheels...");
+		mt = new MotionTask(Priority.SECOND, MotionTask.STOP_SPEED, MotionTask.STOP_HEADING);
+		motionArbiter.submitTask(mt);
+		
+		System.out.println("End of test...");
 		System.exit(0);
 	}
 }
