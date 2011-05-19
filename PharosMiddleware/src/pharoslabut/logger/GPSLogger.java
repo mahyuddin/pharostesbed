13f62a9edc062fc0f8ef248eeaea06758d805cfc
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-05-19 02:03:30 +0000

    Updated to use new GPSDataBuffer API.  Updated logging system to not save debug statements in the log file when not in debug mode.

--- PharosMiddleware/src/pharoslabut/logger/GPSLogger.java
+++ PharosMiddleware/src/pharoslabut/logger/GPSLogger.java
@@ -1,5 +1,6 @@
 package pharoslabut.logger;
 
+import pharoslabut.exceptions.NoNewDataException;
 import pharoslabut.sensors.*;
 
 import playerclient3.GPSInterface;
@@ -13,10 +14,12 @@ import playerclient3.structures.gps.PlayerGpsData;
  * 
  * @author Chien-Liang Fok
  */
-public class GPSLogger implements GPSListener {
+public class GPSLogger implements Runnable {
+	public static final long GPS_LOGGER_REFRESH_PERIOD = 100;
 	
 	private GPSInterface gps = null;
-	private boolean logging;
+	private GPSDataBuffer gpsdb;
+	private boolean logging = false;
 	private long startTime;
 	private FileLogger flogger;
 	
@@ -35,22 +38,21 @@ public class GPSLogger implements GPSListener {
 		this.flogger = flogger;
 		
 		try {
-			log("Connecting to server " + serverIP + ":" + serverPort);
+			logDbg("Connecting to server " + serverIP + ":" + serverPort);
 			client = new PlayerClient(serverIP, serverPort);
 		} catch(PlayerException e) {
-			log("Error connecting to Player: ");
-			log("    [ " + e.toString() + " ]");
+			logErr("Problem connecting to Player server: " + e.toString());
 			System.exit (1);
 		}
 		
 		while (gps == null) {
 			try {
-				log("Subscribing to GPS service...");
+				logDbg("Subscribing to GPS service...");
 				gps = client.requestInterfaceGPS(deviceIndex, PlayerConstants.PLAYER_OPEN_MODE);
 				if (gps != null)
-					log("Subscribed to GPS service...");
+					logDbg("Subscribed to GPS service...");
 				else {
-					log("ERROR: GPS service was null, waiting 1s then retrying...");
+					logErr("GPS service was null, waiting 1s then retrying...");
 					synchronized(this) {
 						try {
 							wait(1000);
@@ -60,22 +62,22 @@ public class GPSLogger implements GPSListener {
 					}
 				}
 			} catch(PlayerException pe) {
-				System.err.println("Error: " + pe.getMessage());
+				logErr(pe.getMessage());
 			}
 		}
 		
 		// The runThreaded and change of data delivery mode can be done in reverse order.
-		log("Setting Player Client to run in continuous threaded mode...");
+		logDbg("Setting Player Client to run in continuous threaded mode...");
 		client.runThreaded(-1, -1);
 		
-		log("Changing Player server mode to PUSH...");
+		logDbg("Changing Player server mode to PUSH...");
 		client.requestDataDeliveryMode(playerclient3.structures.PlayerConstants.PLAYER_DATAMODE_PUSH);
 		
-		log("Creating GPSDataBuffer...");
-		GPSDataBuffer gdb = new GPSDataBuffer(gps);
-		gdb.setFileLogger(flogger);
-		gdb.addGPSListener(this);
-		gdb.start();
+		logDbg("Creating GPSDataBuffer...");
+		gpsdb = new GPSDataBuffer(gps, flogger);
+		
+//		gdb.addGPSListener(this);
+//		gdb.start();
 	}
 	
 	/**
@@ -100,6 +102,8 @@ public class GPSLogger implements GPSListener {
 			
 			startTime = System.currentTimeMillis();
 			
+			new Thread(this).start();
+			
 			return true;
 		} else
 			return false;
@@ -113,23 +117,89 @@ public class GPSLogger implements GPSListener {
 	}
 	
 	@Override
-	public void newGPSData(PlayerGpsData gpsData) {
-		long endTime = System.currentTimeMillis();
-		
-		String result = endTime + "\t" + (endTime - startTime) + "\t";
-		result += gpsData.getQuality()  + "\t" + (gpsData.getLatitude()/1e7) + "\t" 
-			+ (gpsData.getLongitude()/1e7) + "\t" + gpsData.getAltitude() + "\t"
-			+ gpsData.getTime_sec() + "\t" + gpsData.getTime_usec() + "\t"
-			+ gpsData.getErr_vert() + "\t" + gpsData.getErr_horz();
-		
-		log(result);
+//	public void newGPSData(PlayerGpsData gpsData) {
+//		long endTime = System.currentTimeMillis();
+//		
+//		String result = endTime + "\t" + (endTime - startTime) + "\t";
+//		result += gpsData.getQuality()  + "\t" + (gpsData.getLatitude()/1e7) + "\t" 
+//			+ (gpsData.getLongitude()/1e7) + "\t" + gpsData.getAltitude() + "\t"
+//			+ gpsData.getTime_sec() + "\t" + gpsData.getTime_usec() + "\t"
+//			+ gpsData.getErr_vert() + "\t" + gpsData.getErr_horz();
+//		
+//		log(result);
+//	}
+	
+	public void run() {
+		PlayerGpsData gpsData;
+
+		logDbg("run: thread starting...");
+
+		while(logging) {
+			try {
+				gpsData = gpsdb.getCurrLoc();
+				long endTime = System.currentTimeMillis();
+
+				String result = endTime + "\t" + (endTime - startTime) + "\t";
+				result += gpsData.getQuality()  + "\t" + (gpsData.getLatitude()/1e7) + "\t" 
+				+ (gpsData.getLongitude()/1e7) + "\t" + gpsData.getAltitude() + "\t"
+				+ gpsData.getTime_sec() + "\t" + gpsData.getTime_usec() + "\t"
+				+ gpsData.getErr_vert() + "\t" + gpsData.getErr_horz();
+				
+				log(result);
+			} catch (NoNewDataException e1) {
+				logDbg("run: No new data...");
+			}
+
+			try {
+				synchronized(this) {
+					wait(GPS_LOGGER_REFRESH_PERIOD);
+				}
+			} catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		logDbg("run: thread terminating...");
+	}
+
+	/**
+	 * Log debug statements.  These statements are only logged if we are running in 
+	 * debug mode.
+	 * 
+	 * @param msg The debug statement.
+	 */
+	private void logDbg(String msg) {
+		String result = "GPSLogger: " + msg;
+		if (System.getProperty ("PharosMiddleware.debug") != null) {
+			System.out.println(result);
+			if (flogger != null)
+				flogger.log(result);
+		}
 	}
 	
-	private void log(String msg) {
-		String result = "GPSLogger: " + msg;
-		System.out.println(result);
+	/**
+	 * Log error statements.  These are always logged regardless of whether we are running
+	 * in debug mode.
+	 * 
+	 * @param msg The statement.
+	 */
+	private void logErr(String msg) {
+		String result = "GPSLogger: ERROR: " + msg;
+		System.err.println(result);
 		if (flogger != null)
 			flogger.log(result);
+	}
+	
+	/**
+	 * Log statements.  These are always logged regardless of whether we are running
+	 * in debug mode.
+	 * 
+	 * @param msg The statement.
+	 */
+	private void log(String msg) {
+		System.out.println(msg);
+		if (flogger != null)
+			flogger.log(msg);
 	}
 	
 	private static void usage() {
