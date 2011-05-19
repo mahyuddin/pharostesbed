857531fac2a8c7e95bf00bc41e3ffe2dded9df46
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-05-19 01:58:48 +0000

    Simplified by removing thread.  This is because the component that uses this frequently calls getCurrLoc() anyways, so it does not make sense to use a separate thread to grab the GPS data.

--- trunk/PharosMiddleware/src/pharoslabut/sensors/GPSDataBuffer.java
+++ trunk/PharosMiddleware/src/pharoslabut/sensors/GPSDataBuffer.java
@@ -10,22 +10,24 @@ import playerclient3.*;
 import playerclient3.structures.gps.PlayerGpsData;
 
 /**
- * Buffers incoming GPS data.  Implements a thread that cleans up "old" GPS readings, which are readings older
- * than GPS_MAX_AGE.  Provides access to the current GPS data.
+ * Buffers incoming GPS data.  Implements a Pharos-specific buffer for GPS data.
+ * Provides method getCurrLoc() that queries the GPS device for the most recent
+ * GPS measurements.  If such a measurement is not available, it returns the
+ * previous measurement so long as its age is not over GPS_MAX_AGE milliseconds.
  * 
  * @author Chien-Liang Fok
  */
-public class GPSDataBuffer implements Runnable {
+public class GPSDataBuffer {
 	
 	/**
 	 * The period in milliseconds at which to check for expired compass data.
 	 */
-	public static final int GPS_BUFFER_REFRESH_PERIOD = 200;
+//	public static final int GPS_BUFFER_REFRESH_PERIOD = 200;
 	
 	/**
 	 * The maximum age of GPS readings stored in this buffer, in milliseconds.
 	 */
-	public static final int GPS_MAX_AGE = 5000;
+	public static final int GPS_MAX_AGE = 2500;
 	
 	/**
 	 * A proxy to the GPS sensor.
@@ -33,23 +35,37 @@ public class GPSDataBuffer implements Runnable {
 	private GPSInterface gps;
 	
 	/**
-	 * A buffer for storing GPS data.
+	 * A buffer for storing previous GPS measurements that are up to GPS_MAX_AGE old.
 	 */
 	private Vector<GPSDataWrapper> buff = new Vector<GPSDataWrapper>();
 	
-	private boolean running = false;
-	
-	private FileLogger flogger;
-	
-	private Vector<GPSListener> gpsListeners = new Vector<GPSListener>();
+//	private boolean running = false;
 	
 	/**
-	 * The constructor.
+	 * The file logger in which to save log data.
+	 */
+	private FileLogger flogger;
+	
+//	private Vector<GPSListener> gpsListeners = new Vector<GPSListener>();
+	
+	/**
+	 * A constructor.
 	 * 
 	 * @param gps The GPS proxy object.
 	 */
 	public GPSDataBuffer(GPSInterface gps) {
 		this.gps = gps;
+	}
+	
+	/**
+	 * A constructor with a flogger input parameter.
+	 * 
+	 * @param gps The GPS proxy object.
+	 * @param flogger The file logger used to store debug statements.
+	 */
+	public GPSDataBuffer(GPSInterface gps, FileLogger flogger) {
+		this(gps);
+		this.flogger = flogger;
 	}
 	
     /**
@@ -58,36 +74,36 @@ public class GPSDataBuffer implements Runnable {
      * 
      * @param gpsl  The listener to add.
      */
-    public void addGPSListener(GPSListener gpsl) {
-    	gpsListeners.add(gpsl);
-    }
+//    public void addGPSListener(GPSListener gpsl) {
+//    	gpsListeners.add(gpsl);
+//    }
     
     /**
      * Removes a listener from this object.
      * 
      * @param gpsl  The listener to remove.
      */
-    public void removeGPSListener(GPSListener gpsl) {
-    	gpsListeners.remove(gpsl);
-    }
+//    public void removeGPSListener(GPSListener gpsl) {
+//    	gpsListeners.remove(gpsl);
+//    }
     
     /**
      * Notifies each of the registered GPSListener objects that a new PlayerGpsData is available.
      * 
      * @param pgdata The new PlayerGpsData that is available.
      */
-    private void notifyGPSListeners(final PlayerGpsData pgdata) {
-    	if (gpsListeners.size() > 0) {
-    		new Thread(new Runnable() {
-    			public void run() {
-    				Enumeration<GPSListener> e = gpsListeners.elements();
-    	    		while (e.hasMoreElements()) {
-    	    			e.nextElement().newGPSData(pgdata);
-    	    		}
-    			}
-    		}).start();
-    	}
-    }
+//    private void notifyGPSListeners(final PlayerGpsData pgdata) {
+//    	if (gpsListeners.size() > 0) {
+//    		new Thread(new Runnable() {
+//    			public void run() {
+//    				Enumeration<GPSListener> e = gpsListeners.elements();
+//    	    		while (e.hasMoreElements()) {
+//    	    			e.nextElement().newGPSData(pgdata);
+//    	    		}
+//    			}
+//    		}).start();
+//    	}
+//    }
 	
     /**
      * Determines whether the specified location is valid.  For now, it is hard-coded that valid
@@ -120,22 +136,22 @@ public class GPSDataBuffer implements Runnable {
 	/**
 	 * Start the reception of GPS data.
 	 */
-	public synchronized void start() {
-		if (!running) {
-			running = true;
-			new Thread(this).start();
-		}
-	}
+//	public synchronized void start() {
+//		if (!running) {
+//			running = true;
+//			new Thread(this).start();
+//		}
+//	}
 	
 	/**
 	 * Stop the reception of GPS data.
 	 */
-	public synchronized void stop() {
-		if (running) {
-			buff.clear();
-			running = false;
-		}
-	}
+//	public synchronized void stop() {
+//		if (running) {
+//			buff.clear();
+//			running = false;
+//		}
+//	}
 	
 //	private synchronized void add(PlayerGpsData gpsData) {
 //		buff.add(0, gpsData);
@@ -148,9 +164,29 @@ public class GPSDataBuffer implements Runnable {
 	 * 
 	 * @throws NoNewDataException If no GPS data exists in the buffer.
 	 */
-	public synchronized Location getCurrLoc() throws NoNewDataException {
+	public synchronized PlayerGpsData getCurrLoc() throws NoNewDataException {
+		// Grab any new GPS data...
+		if (gps.isDataReady()) {
+			PlayerGpsData newData = gps.getData();
+			buff.add(0, new GPSDataWrapper(newData)); // add new data to the front of the buffer
+			log("getCurrLoc: New GPS Data: " + newData + ", buffer size=" + buff.size());
+			
+			// Estimate the robot's speed
+			if (buff.size() > 1) {
+				Location currLoc = new Location(buff.get(0).getGpsData());
+				Location prevLoc = new Location(buff.get(1).getGpsData());
+				
+				Double dist = currLoc.distanceTo(prevLoc);
+				double time = (buff.get(0).getTimeStamp() - buff.get(1).getTimeStamp()) / 1000.0;
+				log("getCurrLoc: Estimated Speed: " + dist/time + " m/s");
+			}
+		}
+		
+		// Remove any expired GPS location measurements...
+		removeExpiredElements();
+		
 		if (buff.size() > 0)
-			return new Location(buff.get(0).getGpsData());
+			return buff.get(0).getGpsData();
 		else
 			throw new NoNewDataException();
 	}
@@ -168,46 +204,46 @@ public class GPSDataBuffer implements Runnable {
 	/**
 	 * Removes expired GPS readings.
 	 */
-	public void run() {
-		
-		log("run: thread starting...");
-		
-		while(running) {
-			
-			// Grab any new GPS data...
-			if (gps.isDataReady()) {
-				PlayerGpsData newData = gps.getData();
-				buff.add(0, new GPSDataWrapper(newData)); // add new data to the front of the buffer
-				log("run: New GPS Data: " + newData + ", buffer size=" + buff.size());
-				
-				// Estimate the robot's speed
-				if (buff.size() > 1) {
-					Location currLoc = new Location(buff.get(0).getGpsData());
-					Location prevLoc = new Location(buff.get(1).getGpsData());
-					
-					Double dist = currLoc.distanceTo(prevLoc);
-					double time = (buff.get(0).getTimeStamp() - buff.get(1).getTimeStamp()) / 1000.0;
-					log("run: Speed (m/s): " + dist/time);
-				}
-				
-				// Notify the listeners of the new GPS data
-				notifyGPSListeners(newData);
-			}
-			
-			removeExpiredElements();
-			
-			try {
-				synchronized(this) {
-					wait(GPS_BUFFER_REFRESH_PERIOD);
-				}
-			} catch(InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		log("run: thread terminating...");
-	}
-	
+//	public void run() {
+//		
+//		log("run: thread starting...");
+//		
+//		while(running) {
+//			
+//			// Grab any new GPS data...
+//			if (gps.isDataReady()) {
+//				PlayerGpsData newData = gps.getData();
+//				buff.add(0, new GPSDataWrapper(newData)); // add new data to the front of the buffer
+//				log("run: New GPS Data: " + newData + ", buffer size=" + buff.size());
+//				
+//				// Estimate the robot's speed
+//				if (buff.size() > 1) {
+//					Location currLoc = new Location(buff.get(0).getGpsData());
+//					Location prevLoc = new Location(buff.get(1).getGpsData());
+//					
+//					Double dist = currLoc.distanceTo(prevLoc);
+//					double time = (buff.get(0).getTimeStamp() - buff.get(1).getTimeStamp()) / 1000.0;
+//					log("run: Speed (m/s): " + dist/time);
+//				}
+//				
+//				// Notify the listeners of the new GPS data
+//				notifyGPSListeners(newData);
+//			}
+//			
+//			removeExpiredElements();
+//			
+//			try {
+//				synchronized(this) {
+//					wait(GPS_BUFFER_REFRESH_PERIOD);
+//				}
+//			} catch(InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//		}
+//		
+//		log("run: thread terminating...");
+//	}
+//	
 	private void log(String msg) {
 		String result = "GPSDataBuffer: " + msg;
 		if (System.getProperty ("PharosMiddleware.debug") != null)
@@ -218,11 +254,10 @@ public class GPSDataBuffer implements Runnable {
 	
 	/**
 	 * Wraps a GPS location measurement with a time stamp.  This is used instead of
-	 * the time stamp within the PlayerGpsData object since the local clock may
+	 * the time stamp within the PlayerGpsData object since the local system clock may
 	 * not be calibrated.
 	 * 
 	 * @author Chien-Liang Fok
-	 *
 	 */
 	private class GPSDataWrapper {
 		private PlayerGpsData data;
@@ -240,7 +275,7 @@ public class GPSDataBuffer implements Runnable {
 		/**
 		 * An accessor to the GPS data.
 		 * 
-		 * @return The heading.
+		 * @return The location data.
 		 */
 		public PlayerGpsData getGpsData() {
 			return data;
