15762625756539c0ad1e63a6a7c3a99d85269fde
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-07-11 16:51:07 +0000

    Modified to directly read in the motion script.

--- PharosMiddleware/src/pharoslabut/navigate/motionscript/MotionScript.java
+++ PharosMiddleware/src/pharoslabut/navigate/motionscript/MotionScript.java
@@ -1,6 +1,12 @@
 package pharoslabut.navigate.motionscript;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Enumeration;
 import java.util.Vector;
+
+import pharoslabut.navigate.Location;
 
 /**
  * This contains a list of instructions that control the movement and wireless
@@ -10,10 +16,6 @@ import java.util.Vector;
  * @see pharoslabut.navigate.motionscript.Instruction
  */
 public class MotionScript implements java.io.Serializable {
-	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -5210604518776542996L;
 	
 	private Vector<Instruction> instructions = new Vector<Instruction>();;
@@ -28,6 +30,7 @@ public class MotionScript implements java.io.Serializable {
 	 */
 	public MotionScript(String fileName) {
 		this.fileName = fileName;
+		readMotionScript();
 	}
 	
 	/**
@@ -55,9 +58,9 @@ public class MotionScript implements java.io.Serializable {
 	 * 
 	 * @param i The instruction.
 	 */
-	public void addInstruction(Instruction i) {
-		instructions.add(i);
-	}
+//	private void addInstruction(Instruction i) {
+//		instructions.add(i);
+//	}
 	
 	/**
 	 * An accessor to the number of instructions in this motion script.
@@ -66,6 +69,100 @@ public class MotionScript implements java.io.Serializable {
 	 */
 	public int numInstructions() {
 		return instructions.size();
+	}
+	
+	/**
+	 * @return The number of way points in the motion script.
+	 */
+	public int numWayPoints() {
+		int result = 0;
+		Enumeration<Instruction> e = instructions.elements();
+		while (e.hasMoreElements()) {
+			Instruction currInst = e.nextElement();
+			if (currInst.getType().equals(InstructionType.MOVE)) {
+				result++;
+			}
+		}
+		return result;
+	}
+	
+	private void readMotionScript() {
+		try {
+			BufferedReader input =  new BufferedReader(new FileReader(fileName));
+			try {
+				String line = null;
+				int lineno = 1;
+				while (( line = input.readLine()) != null) {
+					if (!line.equals("")  && !line.startsWith("#")  && !line.startsWith("//")) {
+						
+						// Ignore comments...
+						if (line.contains("//"))
+							line = line.substring(0, line.indexOf("//"));
+						
+						try {
+							if (line.contains("MOVE")) {
+								String[] elem = line.split("[\\s]+");
+								double latitude = Double.valueOf(elem[1]);
+								double longitude = Double.valueOf(elem[2]);
+								double speed = Double.valueOf(elem[3]);
+								Location dest = new Location(latitude, longitude);
+								instructions.add(new Move(dest, speed));
+							}
+							else if (line.contains("PAUSE")) {
+								String[] elem = line.split("[\\s]+");
+								long pauseTime = Long.valueOf(elem[1]);
+								instructions.add(new Pause(pauseTime));
+							}
+							else if (line.contains("START_BCAST_TELOSB")) {
+								String[] elem = line.split("[\\s]+");
+								long minPauseTime = Long.valueOf(elem[1]);
+								long maxPauseTime = Long.valueOf(elem[2]);
+								short txPower = Short.valueOf(elem[3]);
+								instructions.add(new StartBcastTelosB(minPauseTime, maxPauseTime, txPower));
+							}
+							else if (line.contains("START_BCAST_WIFI")) {
+								String[] elem = line.split("[\\s]+");
+								long minPauseTime = Long.valueOf(elem[1]);
+								long maxPauseTime = Long.valueOf(elem[2]);
+								short txPower = Short.valueOf(elem[3]);
+								instructions.add(new StartBcastWiFi(minPauseTime, maxPauseTime, txPower));
+							}
+							else if (line.contains("STOP_BCAST_TELOSB")) {
+								instructions.add(new StopBcastTelosB());
+							}
+							else if (line.contains("STOP_BCAST_WIFI")) {
+								instructions.add(new StopBcastWifi());
+							}
+							else if (line.contains("WAIT_EXP_STOP")) {
+								instructions.add(new WaitStopExp());
+							}
+							else if (line.contains("START_SCOOT")) {
+								//String[] elem = line.split("[\\s]+");
+								//int amount = Integer.valueOf(elem[1]);
+								instructions.add(new Scoot());
+							} else if (line.contains("RCV_TELOSB_BEACONS")) {
+								String[] elem = line.split("[\\s]+");
+								int amount = Integer.valueOf(elem[1]);
+								instructions.add(new RcvTelosbBeacons(amount));	
+							}
+							else
+								throw new UnknownInstructionException(line);
+						} catch(Exception e) {
+							e.printStackTrace();
+							System.err.println("Error on line " + lineno + " of motion script " + fileName);
+							System.exit(1);
+						}
+					}
+					lineno++;
+				}
+			}
+			finally {
+				input.close();
+			}
+		}
+		catch (IOException ex){
+			ex.printStackTrace();
+		}
 	}
 	
 	public String toString() {
