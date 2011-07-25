88bba16a86fe335d4719a76139b1c51e3d0b019b
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-07-25 17:10:46 +0000

    Initial commit.

---
+++ trunk/PharosMiddleware/src/pharoslabut/logger/analyzer/VisualizeFaults.java
@@ -0,0 +1,131 @@
+package pharoslabut.logger.analyzer;
+
+import java.util.Vector;
+
+import pharoslabut.logger.FileLogger;
+import pharoslabut.navigate.Location;
+
+public class VisualizeFaults {
+
+	
+	public VisualizeFaults(String logFileName, String outputFileName) {
+		RobotExpData robotData = new RobotExpData(logFileName);
+		
+		FileLogger flogger = new FileLogger(outputFileName, false);
+		
+		// First add a trace of the actual route taken by the robot
+		flogger.log("type,latitude,longitude,name,color"); 
+		Vector<GPSLocationState> locations = robotData.getGPSHistory();
+		for (int j=0; j < locations.size(); j++) {
+			Location currLoc = new Location(locations.get(j).getLoc());
+			String line = "T," + currLoc.latitude() + "," + currLoc.longitude();
+			if (j == 0)
+				line += ", " + robotData.getMissionName() + "-" + robotData.getExpName() + "-" 
+				+ robotData.getRobotName()  + ", blue";
+			flogger.log(line);
+		}
+		
+		// Add the actual waypoints of the motion script
+		Vector<Location> wayPoints = robotData.getWayPoints();
+		flogger.log("type,latitude,longitude,name,color");
+		for (int i=0; i < wayPoints.size(); i++) {
+			Location wpLoc = wayPoints.get(i);
+			flogger.log("W," + wpLoc.latitude() + "," + wpLoc.longitude() + ", Way Point " + i + ",green");
+		}
+		
+		// Add waypoints indicating the locations of GPS sensor Failure
+		Vector<Long> gpsErrors = robotData.getGPSErrors();
+		flogger.log("type,latitude,longitude,name,color");
+		for (int i=0; i < gpsErrors.size(); i++) {
+			long timestamp = gpsErrors.get(i);
+			Location errorLoc = robotData.getLocation(timestamp);
+			flogger.log("W," + errorLoc.latitude() + "," + errorLoc.longitude() + ", GPS Error " + i + ",red");
+		}
+		
+		// Add waypoints indicating the locations of heading sensor Failure
+		Vector<Long> headingErrors = robotData.getHeadingErrors();
+		flogger.log("type,latitude,longitude,name,color");
+		for (int i=0; i < headingErrors.size(); i++) {
+			long timestamp = headingErrors.get(i);
+			Location errorLoc = robotData.getLocation(timestamp);
+			flogger.log("W," + errorLoc.latitude() + "," + errorLoc.longitude() + ", Heading Error " + i + ", yellow");
+		}
+		
+		System.out.println("Number of GPS Faults: " + gpsErrors.size());
+		System.out.println("Number of Heading Faults: " + headingErrors.size());
+	}
+	
+	private void log(String msg) {
+		if (System.getProperty ("PharosMiddleware.debug") != null)
+			System.out.println(msg);
+	}
+	
+	private void logErr(String msg) {
+		System.err.println(msg);
+	}
+	
+	private static void print(String msg) {
+		System.out.println(msg);
+	}
+	
+	private static void printErr(String msg) {
+		System.err.println(msg);
+	}
+	
+	private static void usage() {
+		print("Usage: pharoslabut.logger.analyzer.VisualizeFaults <options>\n");
+		print("Where <options> include:");
+		print("\t-log <log file>: The log file to analyze (required)");
+		print("\t-output <output file>: Where to save the results. (required)");
+		print("\t-d: enable debug mode");
+	}
+	
+	public static void main(String[] args) {
+		String logFileName = null;
+		String outputFileName = null;
+		
+		try {
+			for (int i=0; i < args.length; i++) {
+				if (args[i].equals("-h") || args[i].equals("-help")) {
+					usage();
+					System.exit(0);
+				}
+				
+				if (args[i].equals("-log")) {
+					logFileName = args[++i];
+				}
+				else if (args[i].equals("-output")) {
+					outputFileName = args[++i];
+				}
+				else if (args[i].equals("-debug") || args[i].equals("-d")) {
+					System.setProperty ("PharosMiddleware.debug", "true");
+				}
+				else {
+					printErr("Unknown option: " + args[i]);
+					usage();
+					System.exit(1);
+				}
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+			usage();
+			System.exit(1);
+		}
+		
+		if (logFileName == null || outputFileName == null) {
+			printErr("Must specify log file and output file.");
+			usage();
+			System.exit(1);
+		}
+		
+		print("log file: " + logFileName);
+		print("output file: " + outputFileName);
+		print("Debug: " + (System.getProperty ("PharosMiddleware.debug") != null));
+		
+		try {
+			new VisualizeFaults(logFileName, outputFileName);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
