fe59eddbe364d36aa56c7a712375dc0f76586d4f
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-07-25 17:34:32 +0000

    Added constructor that accepts Location object.

--- trunk/PharosMiddleware/src/pharoslabut/logger/analyzer/WayPoint.java
+++ trunk/PharosMiddleware/src/pharoslabut/logger/analyzer/WayPoint.java
@@ -2,12 +2,23 @@ package pharoslabut.logger.analyzer;
 
 import pharoslabut.navigate.Location;
 
+/**
+ * A waypoint is a target location that the robot should reach.  
+ * 
+ * @author Chien-Liang
+ *
+ */
 public class WayPoint {
 	private Location loc;
 	String name;
 	
 	public WayPoint(double lat, double lon, String name) {
 		loc = new Location(lat, lon);
+		this.name = name;
+	}
+	
+	public WayPoint(Location loc, String name) {
+		this.loc = loc;
 		this.name = name;
 	}
 	
