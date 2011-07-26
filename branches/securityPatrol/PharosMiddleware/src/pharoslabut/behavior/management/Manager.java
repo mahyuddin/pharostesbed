b5c115d5c5367b7267a5ecf8c469ad36a2292a1c
noagmon <noagmon@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-07-26 02:25:59 +0000

    made changes for dynamic management

--- branches/securityPatrol/PharosMiddleware/src/pharoslabut/behavior/management/Manager.java
+++ branches/securityPatrol/PharosMiddleware/src/pharoslabut/behavior/management/Manager.java
@@ -9,8 +9,6 @@ import pharoslabut.behavior.*;
 import pharoslabut.io.*;
 import pharoslabut.logger.FileLogger;
 import pharoslabut.navigate.*;
-import pharoslabut.behavior.communication.*;
-import pharoslabut.behavior.fileParsing.ReadWithScanner;
 import pharoslabut.demo.mrpatrol.*;
 import pharoslabut.exceptions.PharosException;
 
@@ -29,16 +27,18 @@ public class Manager {
 	NavigateCompassGPS _NavigateData;
 	/* need to know whether to connect the last behavior to the first, and if so - for how many repeats */
 	private int _CircularRepeats; 
+	private boolean _manageDynamic;
 	
 	private TCPMessageSender _sender;
 	
-	public Manager(MRPConfData mrpConfdata, NavigateCompassGPS navigationdata, TCPMessageSender sender, FileLogger flogger){
+	public Manager(MRPConfData mrpConfdata, NavigateCompassGPS navigationdata, TCPMessageSender sender, FileLogger flogger, boolean manageDynamic){
 		_wm = new WorldModel(mrpConfdata.GetNumRobots(), mrpConfdata.GetMyindex());
 		_behVect = new Vector<Behavior>();
 		_CircularRepeats = mrpConfdata.CircularRepeat();
 		_NavigateData = navigationdata;
 		_sender = sender;
 		_flogger = flogger;
+		_manageDynamic = manageDynamic;
 		
 		System.out.print("Manager: number of robots:"+mrpConfdata.GetNumRobots()+" nummissions: "+mrpConfdata.GetNumMissions());
 		for(int index = 0; index<mrpConfdata.GetNumRobots(); index++){
@@ -81,107 +81,19 @@ public class Manager {
 		
 		
 		log("_behVect: "+ _current.getClass().getName()+_currentIndex);
-		_wm.setCurrentBehaviorName((_current.getClass().getName())+_currentIndex);
-		
-//		_server = new Server(_wm);
-//		_server.start();
-//		_client = new Client(_wm);
-//		_client.start();
+		_wm.setMyCurrentBehavior((_current.getClass().getName())+_currentIndex, _currentIndex);
 	}
 	
 	
-//	public Manager(String configFile_team, String configFile_Behave) throws NumberFormatException, FileNotFoundException
-//	{
-//		ReadWithScanner fileParser = new ReadWithScanner(configFile_team);
-//		int teamSize = Integer.parseInt((fileParser.getParameterValue("teamSize")).trim());
-//		int myIndex = Integer.parseInt((fileParser.getParameterValue("myIndex")).trim());
-//		_wm = new WorldModel(teamSize, myIndex);
-//		fileParser.getTeamIpPort(_wm);
-//		_robot = new Robot(_wm.getMyIp(),_wm.getMyPort());
-//		
-//		initialize_behaviors(configFile_Behave);
-//		
-//		_current = _behVect.get(0);
-//		_currentIndex = 0;
-//		_wm.setCurrentBehaviorName((_current.getClass().getName())+_currentIndex);
-//		log("Manager: setting behavior to "+_current.getClass().getName()+_currentIndex);
-//		
-////		_server = new Server(_wm);
-////		_server.start();
-////		_client = new Client(_wm);
-////		_client.start();
-//	}
+
 	
-//	private void initialize_behaviors(String configFile_Behave){
-//		//Creating the behaviors
-//		ReadWithScanner fileParser = new ReadWithScanner(configFile_Behave);
-//		int numoftargets = 0;
-//		int numofrounds = 0;
-//		
-//		try {
-//			numoftargets = Integer.parseInt((fileParser.getParameterValue("NumTargets")).trim());
-//		} catch (NumberFormatException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//			logErr("Error in parsing int");
-//		} catch (FileNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		try {
-//			numofrounds = Integer.parseInt((fileParser.getParameterValue("NumRounds")).trim());
-//		} catch (NumberFormatException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (FileNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		_CircularRepeats = numofrounds;
-//		_missionData = new MissionData[numoftargets];
-//		
-//		try {
-//			fileParser.getLatitudeLongitude(_missionData, numoftargets);
-//		} catch (FileNotFoundException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//		_behVect = new Vector<Behavior>();
-//		for(int j=0;j<numoftargets; j++){
-//			_behVect.add(new BehGotoGPSCoord(_wm, _missionData[j], _NavigateData, _flogger));
-//			_behVect.get(j).BehSetIndex(j);
-//			// add the circular behavior nature
-//			if(j>0){
-//				_behVect.get(j-1).addNext(_behVect.get(j));
-//			}
-//		}
-//		// make it circular - connect the last to the first
-//		if(_CircularRepeats > 0)
-//			_behVect.get(numoftargets-1).addNext(_behVect.get(0));
-//		
-//	}
-	
-	public void updateTeammates(String behavename, int teammateID){
-		_wm.setTeamCurrentBehavior(behavename, teammateID);
+	public void updateTeammates(String behavename, int teammateID, int behaveID){
+		_wm.setTeamCurrentBehavior(behavename, teammateID, behaveID);
 	}
 	
 	public void run()
 	{
 
-//		while(_wm.isAllMyClientsConnected() == false || _wm.isConnectToAllServers() == false)
-//		{
-//			System.out.println("waiting for teammates isallmyclientsconnected (\n"+_wm.isAllMyClientsConnected()+") isConnectToAllServers ("+_wm.isConnectToAllServers());
-//			try {
-//				synchronized(this){
-//					wait(100);
-//				}
-//			} catch (InterruptedException e) {
-//				// TODO Auto-generated catch block
-//				logErr("Manager.run(): Failed to Sleep ");
-//				e.printStackTrace();
-//			}
-//		}
 		if(_current.startCondition() == false)
 		{
 			log("Program finished");
@@ -190,7 +102,7 @@ public class Manager {
 		//update that we established a connection to all servers, and are now able to continue
 		sendBehaviorToClients();
 		//If you want your robot to be synchronized when behavior changed, run this method.
-		_current.waitToTeam();
+		waitToTeam();
 
 		int numberRounds = _CircularRepeats*_behVect.size();
 		while(_current != null)
@@ -215,10 +127,10 @@ public class Manager {
 			}
 			log("Manager.run(): End behavior");
 			
-			_wm.setCurrentBehaviorName("stop"+(_current.getClass().getName())+_currentIndex);
+			_wm.setMyCurrentBehavior("stop"+(_current.getClass().getName())+_currentIndex, _currentIndex);
 			sendBehaviorToClients();
 			log("Sending -- stop"+_current.getClass().getName()+_currentIndex+" to my clients");
-			_current.waitToTeam();
+			waitToTeam();
 			
 			
 			
@@ -228,9 +140,9 @@ public class Manager {
 
 			_currentIndex = _current.BehGetIndex();
 			
-			_wm.setCurrentBehaviorName(_current.getClass().getName()+_currentIndex);
+			_wm.setMyCurrentBehavior(_current.getClass().getName()+_currentIndex, _currentIndex);
 			sendBehaviorToClients();
-			_current.waitToTeam();
+			waitToTeam();
 			
 			if(numberRounds<=0)
 				break;
@@ -247,7 +159,7 @@ public class Manager {
 		MultiRobotBehaveMsg msg = null;
 		
 		try {
-			msg = new MultiRobotBehaveMsg(_wm.getCurrentBehaviorName(), _wm.getMyIndex(), InetAddress.getByName(_wm.getMyIp()), _wm.getMyPort());
+			msg = new MultiRobotBehaveMsg(_wm.getCurrentBehaviorName(), _wm.getCurrentBehaviorID(), _wm.getMyIndex(), InetAddress.getByName(_wm.getMyIp()), _wm.getMyPort());
 		} catch(Exception e) {
 			e.printStackTrace();
 			System.exit(1);
@@ -269,8 +181,30 @@ public class Manager {
 				}
 				//_out[i].println(_wm.getCurrentBehaviorName());
 				
-				System.out.println("AFTER Send: "+_wm.getCurrentBehaviorName()+ " Sent to Client id: "+i);
+				log("AFTER Send: "+_wm.getCurrentBehaviorName()+ " Sent to Client id: "+i);
 			}
+	}
+
+	public void waitToTeam()
+	{
+		log("Waiting for teammates\n");
+		boolean continueLoop;
+		
+		if(_manageDynamic) {
+			_wm.checkAliveTeam();
+			continueLoop = _wm.isTeamSynchronizedDynamically();
+		}else
+			continueLoop = _wm.isTeamSynchronized() ;
+
+		while( continueLoop == false) {
+			try {
+				Thread.currentThread().sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			continueLoop = _manageDynamic? _wm.isTeamSynchronizedDynamically() : _wm.isTeamSynchronized() ;
+		}
 	}
 
 	private void logErr(String msg) {
