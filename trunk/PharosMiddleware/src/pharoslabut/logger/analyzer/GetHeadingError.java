626fe82241079f4657e6f309ee4aea51383155c9
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-08-10 18:26:28 +0000

    Debugged GetHeadingError.  Tested on M26-Exp1 Lonestar data.

--- trunk/PharosMiddleware/src/pharoslabut/logger/analyzer/GetHeadingError.java
+++ trunk/PharosMiddleware/src/pharoslabut/logger/analyzer/GetHeadingError.java
@@ -9,11 +9,11 @@ import org.jfree.ui.*;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.NumberAxis;
+//import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
-import org.jfree.data.Range;
+//import org.jfree.data.Range;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
@@ -32,7 +32,7 @@ public class GetHeadingError {
 
 	private String logFileName;
 	
-	RobotExpData robotData;
+	private RobotExpData robotData;
 	
 	private Vector<HeadingDivergenceState> divergenceData = new Vector<HeadingDivergenceState>();
 	
@@ -40,30 +40,69 @@ public class GetHeadingError {
 	 * The constructor.
 	 * 
 	 * @param logFileName The experiment log file to analyze.
+	 * @param saveToFile whether to save the error calculations to a file.
 	 */
-	public GetHeadingError(String logFileName) {
+	public GetHeadingError(String logFileName, boolean saveToFile) {
 		this.logFileName = logFileName;
 		robotData = new RobotExpData(logFileName);
-		//FileLogger flogger = new FileLogger(outputFileName, false);
 		
 		long startTime = robotData.getStartTime();
 		
-		//log("Time (ms)\tDelta Time (ms)\tActual Heading\tIdeal Heading\tHeading Error", flogger);
-		
 		// Calculate heading divergence every 1s
 		for (long time = startTime; time < robotData.getStopTime(); time += HEADING_ERROR_CALCULATION_INTERVAL) {
-			double currHeading = robotData.getHeading(time);
-			double idealHeading = robotData.getIdealHeading(time);
-			double headingError = pharoslabut.navigate.Navigate
-				.headingError(currHeading, idealHeading);
-			
-			divergenceData.add(new HeadingDivergenceState(time, currHeading, idealHeading, headingError));
-			//log(time + "\t" + (time - startTime) + "\t" + currHeading + "\t" + idealHeading 
-			//		+ "\t" + headingError, flogger);
+			divergenceData.add(getHeadingError(time));
 		}
 		
+		if (saveToFile)
+			saveToFile();
 		showPlot();
+	}
+	
+	private void saveToFile() {
+		String fileName;
+		if (logFileName.contains("."))
+			fileName = logFileName.substring(logFileName.lastIndexOf('.')) + "-headingError.txt";
+		else
+			fileName = logFileName + "-headingError.txt";
 		
+		FileLogger flogger = new FileLogger(fileName, false);
+		long startTime = robotData.getStartTime();
+		
+		log("Time (ms)\tDelta Time (ms)\tActual Heading\tIdeal Heading\tHeading Error", flogger);
+		Enumeration<HeadingDivergenceState> e = divergenceData.elements();
+		while (e.hasMoreElements()) {
+			HeadingDivergenceState currState = e.nextElement();
+			log(currState.time 
+					+ "\t" + (currState.time - startTime) 
+					+ "\t" + currState.currHeading 
+					+ "\t" + currState.idealHeading 
+					+ "\t" + currState.headingError, flogger);
+		}
+		
+		// Print when the robot starts traversing an edge...
+		log("Start Times of Edge Traversal:", flogger);
+		log("Time (ms)\tDelta Time (ms)\tDummy", flogger);
+		Vector<PathEdge> pathEdges = robotData.getPathEdges();
+		Enumeration<PathEdge> e2 = pathEdges.elements();
+		while (e2.hasMoreElements()) {
+			PathEdge currEdge = e2.nextElement();
+			long relStartTime = (currEdge.getStartTime() - robotData.getStartTime())/1000;
+			log(currEdge.getStartTime() + "\t" + relStartTime + "\t" + 0, flogger);
+		}
+	}
+	
+	/**
+	 * Calculates the heading error of the robot at the specified time.
+	 * 
+	 * @param time The time to consider.
+	 * @return The heading error of the robot.
+	 */
+	private HeadingDivergenceState getHeadingError(long time) {
+		double currHeading = robotData.getHeading(time);
+		double idealHeading = robotData.getIdealHeading(time);
+		double headingError = pharoslabut.navigate.Navigate
+			.headingError(currHeading, idealHeading);
+		return new HeadingDivergenceState(time, currHeading, idealHeading, headingError);
 	}
 	
 	/**
@@ -71,25 +110,39 @@ public class GetHeadingError {
 	 */
 	private void showPlot() {
 		// Create a data series containing the heading error data...
-		XYSeries dataSeries = new XYSeries("Heading Divergence Measurements");
+		XYSeries headingErrorSeries = new XYSeries("Heading Error");
 		Enumeration<HeadingDivergenceState> e = divergenceData.elements();
 		while (e.hasMoreElements()) {
 			HeadingDivergenceState currState = e.nextElement();
-			dataSeries.add((currState.time - robotData.getStartTime())/1000, currState.headingError);
+			headingErrorSeries.add((currState.time - robotData.getStartTime())/1000, currState.headingError);
+		}
+		
+		// Create a data series containing the times when the robot starts heading towards a waypoint
+		XYSeries waypointSeries = new XYSeries("Begin Edge Traveral");
+		Vector<PathEdge> pathEdges = robotData.getPathEdges();
+		Enumeration<PathEdge> e2 = pathEdges.elements();
+		while (e2.hasMoreElements()) {
+			PathEdge currEdge = e2.nextElement();
+			long waypointTime = (currEdge.getStartTime() - robotData.getStartTime())/1000;
+//			HeadingDivergenceState headingErrorState = getHeadingError(currEdge.getEndTime());
+			
+			waypointSeries.add(waypointTime, 0); //headingErrorState.headingError);
+//			log(waypointArrivalTime + ", " + headingErrorState.headingError, null);
 		}
 
 		// Create a dataset out of the data series
 		XYSeriesCollection dataset = new XYSeriesCollection();
-		dataset.addSeries(dataSeries);
+		dataset.addSeries(headingErrorSeries);
+		dataset.addSeries(waypointSeries);
 		
 		// Create the chart
         JFreeChart chart = ChartFactory.createXYLineChart(
-            "Heading Error Measurements",        // chart title
+            "Heading Error vs. Time",            // chart title
             "Time (s)",                          // x axis label
             "Heading Error (radians)",           // y axis label
             dataset,                             // the data
             PlotOrientation.VERTICAL,            // plot orientation (y axis is vertical)
-            false,                               // include legend
+            true,                                // include legend
             true,                                // tooltips
             false                                // urls
         );
@@ -102,10 +155,14 @@ public class GetHeadingError {
 //        plot.setDomainGridlinePaint(Color.white);
 //        plot.setRangeGridlinePaint(Color.white);
         
+        // Display the points and not the lines connecting the points
         XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
-        renderer.setSeriesLinesVisible(0, false);
-        renderer.setSeriesShapesVisible(1, false);
+        renderer.setSeriesLinesVisible(0, true); // display the heading errors as a line
+        renderer.setSeriesShapesVisible(0, false);
+        renderer.setSeriesLinesVisible(1, false); // display the waypoints as a point
+        renderer.setSeriesShapesVisible(1, true);
         plot.setRenderer(renderer);
+        
         
 //        final NumberAxis domainAxis = (NumberAxis)plot.getDomainAxis();
 //        domainAxis.setRange(new Range(0,140));
@@ -117,9 +174,10 @@ public class GetHeadingError {
 //        rangeAxis.setRange(new Range(-Math.PI, Math.PI));     
         
         ChartPanel chartPanel = new ChartPanel(chart);
-        chartPanel.setPreferredSize(new java.awt.Dimension(1000, 400));
+        chartPanel.setPreferredSize(new java.awt.Dimension(1000, 600));
        
        
+        // Create a frame for the chart, then display it.
         ApplicationFrame appFrame = new ApplicationFrame("Heading Error for " + logFileName);
         appFrame.setContentPane(chartPanel);
         appFrame.pack();
@@ -161,7 +219,7 @@ public class GetHeadingError {
 //	}
 	
 	private void log(String msg, FileLogger flogger) {
-		String result = "HeadingDivergence: " + msg;
+		String result = getClass().getName() + ": " + msg;
 		System.out.println(result);
 		if (flogger != null)
 			flogger.log(msg);
@@ -190,17 +248,22 @@ public class GetHeadingError {
 		print("Usage: " + GetHeadingError.class.getName()  + " <options>\n");
 		print("Where <options> include:");
 		print("\t-log <log file name>: The log file generated during the experiment. (required)");
+		print("\t-save: Save the heading errors in a text file.  The name will be the same as the log file but ending with \"-headingError.txt\" (optional)");
 		print("\t-debug or -d: Enable debug mode");
 	}
 	
 	public static void main(String[] args) {
 		String logFileName = null;
+		boolean saveToFile = false;
 		
 		try {
 			for (int i=0; i < args.length; i++) {
 				if (args[i].equals("-h") || args[i].equals("-help")) {
 					usage();
 					System.exit(0);
+				}
+				else if (args[i].equals("-save")) {
+					saveToFile = false;
 				}
 				else if (args[i].equals("-log")) {
 					logFileName = args[++i];
@@ -230,7 +293,7 @@ public class GetHeadingError {
 		print("Debug: " + (System.getProperty ("PharosMiddleware.debug") != null));
 		
 		try {
-			new GetHeadingError(logFileName);
+			new GetHeadingError(logFileName, saveToFile);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
