49ce2e4f941e008f2ccab711be42f9ee38c04333
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-08-27 20:01:09 +0000

    Modified to be compatible with new logging syntax.  Should maintain backwards compatibility with old loggin syntax.

--- trunk/PharosMiddleware/src/pharoslabut/navigate/MotionScriptFollower.java
+++ trunk/PharosMiddleware/src/pharoslabut/navigate/MotionScriptFollower.java
@@ -118,7 +118,7 @@ public class MotionScriptFollower implements Runnable {
 	private boolean handleMove(Move moveInstr) {
 		boolean result = true;
 		
-		Logger.log("Going to " + moveInstr.getDest() + " at " + moveInstr.getSpeed() + " m/s");
+		Logger.log("Going to " + moveInstr.getDest() + " at " + moveInstr.getSpeed() + " m/s name = " + moveInstr.getWaypointName());
 
 		// The following is a blocking operation.  It does not return until the robot is at the destination.
 		if (navigator.go(moveInstr.getDest(), moveInstr.getSpeed())) { 
