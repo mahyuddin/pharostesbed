f49c355aa1ef6588d4d46538736a6758da3a0532
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-09-08 21:45:22 +0000

    Added IR-based intersection detector.

--- PharosMiddleware/src/pharoslabut/demo/autoIntersection/TestIntersectionDetector.java
+++ PharosMiddleware/src/pharoslabut/demo/autoIntersection/TestIntersectionDetector.java
@@ -1,5 +1,18 @@
 package pharoslabut.demo.autoIntersection;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.font.LineMetrics;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 import pharoslabut.logger.FileLogger;
 import pharoslabut.logger.Logger;
 import pharoslabut.navigate.LineFollower;
@@ -17,7 +30,10 @@ import pharoslabut.navigate.LineFollower;
 public class TestIntersectionDetector implements IntersectionEventListener {
 	public static enum IntersectionDetectorType {BLOB, CRICKET, IR};
 
-	public TestIntersectionDetector(String serverIP, int serverPort, String serialPort, IntersectionDetectorType detectorType) {
+	private IntesectionDetectorDisplay display = null;
+	
+	public TestIntersectionDetector(String serverIP, int serverPort, String serialPort, 
+			IntersectionDetectorType detectorType, boolean showGUI) {
 		LineFollower lf = new LineFollower(serverIP, serverPort);
 
 		if (detectorType == IntersectionDetectorType.BLOB) {
@@ -33,16 +49,112 @@ public class TestIntersectionDetector implements IntersectionEventListener {
 		}
 		else if (detectorType == IntersectionDetectorType.IR) {
 			Logger.log("Testing the IR intersection detector.");
-			IntersectionDetectorIR id = new IntersectionDetectorIR(lf.getPlayerClient());
+			IntersectionDetectorIR id = new IntersectionDetectorIR(lf.getOpaqueInterface());
 			id.addIntersectionEventListener(this);
 		}
+		
+		if (showGUI)
+			display = new IntesectionDetectorDisplay();
 		lf.start();
 	}
 	
 	@Override
 	public void newIntersectionEvent(IntersectionEvent ie) {
 		Logger.log("**** INTERSECTION EVENT: " + ie);
+		if (display != null)
+			display.updateText(ie.getType().toString());
+	}
+	
+	private class IntesectionDetectorDisplay extends JLabel {
+		public static final long DISPLAY_DURATION = 2000;
 		
+		private static final long serialVersionUID = -2931497139130826529L;
+
+		private int tracking = 0;
+
+		private int left_x = 1, left_y = 1, right_x = 1, right_y = 1;
+
+		private Color left_color = Color.WHITE, right_color = Color.WHITE;
+
+		Timer timer;
+		
+		public IntesectionDetectorDisplay() {
+			super("No Intersection");
+			setForeground(Color.black);
+			setFont(getFont().deriveFont(140f));
+
+
+			JFrame frame = new JFrame("Intersection Detector");
+			frame.getContentPane().add(this);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.pack();
+			frame.setLocationRelativeTo(null); // center frame
+			frame.setVisible(true);
+		}
+		
+		public void clearText() {
+			setText("            ");
+		}
+		
+		public void updateText(String msg) {
+			if (timer != null) {
+				timer.cancel();
+				timer = null;
+			}
+			setText(msg);
+			timer = new Timer();
+			timer.schedule(new TimerTask() {
+				public void run() {
+					clearText();
+					timer = null;
+				}
+			}, DISPLAY_DURATION);
+		}
+
+		public Dimension getPreferredSize() {
+			String text = getText();
+			FontMetrics fm = this.getFontMetrics(getFont());
+
+			int w = fm.stringWidth(text);
+			w += (text.length() - 1) * tracking;
+			w += left_x + right_x;
+
+			int h = fm.getHeight();
+			h += left_y + right_y;
+
+			return new Dimension(w, h);
+		}
+
+		public void paintComponent(Graphics g) {
+			((Graphics2D) g).setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+					RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+			char[] chars = getText().toCharArray();
+
+			FontMetrics fm = this.getFontMetrics(getFont());
+			int h = fm.getAscent();
+			LineMetrics lm = fm.getLineMetrics(getText(), g);
+			g.setFont(getFont());
+
+			int x = 0;
+
+			for (int i = 0; i < chars.length; i++) {
+				char ch = chars[i];
+				int w = fm.charWidth(ch) + tracking;
+
+				g.setColor(left_color);
+				g.drawString("" + chars[i], x - left_x, h - left_y);
+
+				g.setColor(right_color);
+				g.drawString("" + chars[i], x + right_x, h + right_y);
+
+				g.setColor(getForeground());
+				g.drawString("" + chars[i], x, h);
+
+				x += w;
+			}
+
+		}
 	}
 	
 	private static void print(String msg) {
@@ -59,6 +171,7 @@ public class TestIntersectionDetector implements IntersectionEventListener {
 		print("\t-log <log file name>: The name of the file in which to save debug output (default null)");
 		print("\t-serial <port>: The serial port to which the cricket is attached (default /dev/ttyS1)");
 		print("\t-type <detector type>: The type of detector to use (blob, cricket, ir, default ir)");
+		print("\t-gui: show the GUI.");
 		print("\t-debug: enable debug mode");
 	}
 	
@@ -68,6 +181,7 @@ public class TestIntersectionDetector implements IntersectionEventListener {
 		boolean useBlobFinder = false;
 		String cricketSerialPort = "/dev/ttyS1";
 		IntersectionDetectorType detectorType = IntersectionDetectorType.IR;
+		boolean showGUI = false;
 		
 		try {
 			for (int i=0; i < args.length; i++) {
@@ -91,6 +205,8 @@ public class TestIntersectionDetector implements IntersectionEventListener {
 						detectorType = IntersectionDetectorType.CRICKET; 
 				} else if (args[i].equals("-log")) {
 					Logger.setFileLogger(new FileLogger(args[++i]));
+				}  else if (args[i].equals("-gui")) {
+					showGUI = true;
 				} else if (args[i].equals("-h")) {
 					usage();
 					System.exit(0);
@@ -108,6 +224,6 @@ public class TestIntersectionDetector implements IntersectionEventListener {
 		print("Server IP: " + serverIP);
 		print("Server port: " + serverPort);
 		
-		new TestIntersectionDetector(serverIP, serverPort, cricketSerialPort, detectorType);
+		new TestIntersectionDetector(serverIP, serverPort, cricketSerialPort, detectorType, showGUI);
 	}
 }
