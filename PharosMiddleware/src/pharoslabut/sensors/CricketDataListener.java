849f136efa3fd253b464c3cb39669cf49348fecd
kevinaboos <kevinaboos@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-09-28 06:53:28 +0000

    added javadoc

--- PharosMiddleware/src/pharoslabut/sensors/CricketDataListener.java
+++ PharosMiddleware/src/pharoslabut/sensors/CricketDataListener.java
@@ -1,5 +1,16 @@
 package pharoslabut.sensors;
 
+/**
+ * Defines the interface that all CricketDataListeners must implement.
+ * 
+ * @author Kevin Boos
+ */
 public interface CricketDataListener {
+	
+	/**
+	 * This is called by CricketDataInterface whenever new CricketData is received.
+	 * 
+	 * @param data The new data received.
+	 */
 	public void newCricketData(CricketData cd);
 }
