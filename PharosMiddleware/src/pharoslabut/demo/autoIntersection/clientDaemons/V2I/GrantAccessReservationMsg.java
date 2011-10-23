d7d22efcbce4b06153f845b5edaa8e24671a51d9
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-10-23 00:04:37 +0000

    Initial Commit.

---
+++ PharosMiddleware/src/pharoslabut/demo/autoIntersection/clientDaemons/V2I/GrantAccessReservationMsg.java
@@ -0,0 +1,36 @@
+package pharoslabut.demo.autoIntersection.clientDaemons.V2I;
+
+import java.net.InetAddress;
+
+/**
+ * Sent by the server to the robot assigning it a time in the future when it may
+ * enter the intersection.
+ * 
+ * @author Chien-Liang Fok
+ */
+public class GrantAccessReservationMsg extends GrantAccessMsg {
+	
+	private static final long serialVersionUID = -8445310513843598319L;
+	
+	private long reservationTime;
+	
+	/**
+	 * The constructor.
+	 * 
+	 * @param robotIP The IP address of the vehicle.
+	 * @param robotPort The port of the vehicle.
+	 * @param reservationTime The time in the future when the vehicle is allowed to enter the intersection.
+	 */
+	public GrantAccessReservationMsg(InetAddress robotIP, int robotPort, long reservationTime) {
+		super(robotIP, robotPort);
+		this.reservationTime = reservationTime;
+	}
+	
+	public long getReservationTime() {
+		return reservationTime;
+	}
+	
+	public String toString() {
+		return getClass().getName() + ", reservationTime = " + reservationTime;
+	}
+}
