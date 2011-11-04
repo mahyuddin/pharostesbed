941ee64ef793539fd68b8c2bb703d772adc9cb90
kevinaboos <kevinaboos@53a56b47-fd32-4597-bdd3-0ed485b86959>
2011-11-04 07:54:35 +0000

    Basic Assertion Framework is finished. Still needs testing on actual hardware with actual sensors.

--- PharosMiddleware/src/pharoslabut/cps/CmdExec.java
+++ PharosMiddleware/src/pharoslabut/cps/CmdExec.java
@@ -5,7 +5,6 @@ import java.awt.image.*;
 import java.io.*;
 import java.net.*;
 
-import pharoslabut.demo.simonsays.io.*;
 import pharoslabut.exceptions.PharosException;
 import pharoslabut.io.*;
 import pharoslabut.logger.*;
@@ -92,8 +91,9 @@ public class CmdExec implements MessageReceiver {
 			return false;
 		}
 		
-//		if (msg instanceof RobotMoveMsg) // movement starts the timer
-//			bdc.startTimer();
+		// no need to wait for an acknowledgement when sending an AssertionRequestMsg
+		if(msg instanceof AssertionRequestMsg)
+			return true;
 		
 		boolean result = false;
 		
@@ -124,9 +124,6 @@ public class CmdExec implements MessageReceiver {
 				Logger.logErr("Ack not received...");
 			}
 		}
-		
-//		if (result)
-//			bdc.stopTimer();
 			
 		return result;
 	}
@@ -136,15 +133,31 @@ public class CmdExec implements MessageReceiver {
 		synchronized(this) {
 			this.rcvMsg = msg;
 			
-			// TODO if (rcvMsg instanceof AssertionResultMsg) 
-			// TODO 	handleReceivedAssertion();
-			// TODO		result = true;
-			
-			// else {
+			if (rcvMsg instanceof AssertionResponseMsg) {
+				handleReceivedAssertion((AssertionResponseMsg)rcvMsg);
+			}
+			else {
 				this.notifyAll();
-			//}
+			}
 		}
 	}
+	
+	
+	/**
+	 * Performs the actions that should be taken when a AssertionResponseMsg is received.
+	 * 
+	 * @param sdMsg The AssertionResponseMsg received from the client
+	 */
+	private void handleReceivedAssertion(AssertionResponseMsg arMsg) {
+		arMsg.msgReceived();
+		
+		System.out.println(arMsg.getAssertionMsg());
+		
+		
+		
+	}
+	
+	
 	
 	/**
 	 * Moves the robot forward or backward.  This is a blocking operation meaning the calling thread
@@ -172,151 +185,4 @@ public class CmdExec implements MessageReceiver {
 		return sendMsg(new PlayerControlMsg(PlayerControlCmd.START));
 	}
 	
-	/**
-	 * Shuts down the player server.
-	 */
-//	public void resetPlayer() {
-//		log("Resetting the player server...");
-//		ResetPlayerMsg msg = new ResetPlayerMsg();
-//		tcpSender.sendMessage(msg);
-//	}
-	
-	/**
-	 * Turns the robot right or left.  This is a blocking operation meaning the calling thread
-	 * blocks and does not return until the operation is complete.
-	 * 
-	 * @param angle The angle to turn in degrees.  The valid range is from
-	 * -360 to 360.  Negative is to turn right (clockwise), positive is to 
-	 * turn left (counter-clockwise).
-	 * @return whether the operation was successful.
-	 */
-//	public boolean turnRobot(double angle) {
-//		
-//		// Some sanity checks...
-//		if (angle < -360 || angle > 360) {
-//			log("turnRobot: ERROR: Invalid turn angle (" + angle + ")...");
-//			return false;
-//		}
-//		
-//		log("turnRobot: Sending turn command (" + angle + ") to DemoServer...");
-//		return sendMsg(new RobotTurnMsg(angle));
-//	}
-	
-	/**
-	 * Pans the camera side to side.
-	 * 
-	 * @param angle The pan angle in degrees.  The valid range is from
-	 * -45 to 45.  Zero degrees is straight forward. Negative is towards the
-	 * left, positive is towards the right.
-	 * @return whether the operation was successful.
-	 */
-//	public boolean panCamera(double angle) {
-//		
-//		// Some sanity checks...
-//		if (angle < -45 || angle > 45) {
-//			log("panCamera: Invalid camera pan angle (" + angle + ")...");
-//			return false;
-//		}
-//		
-//		log("panCamera: Sending pan camera command to DemoServer...");
-//		return sendMsg(new CameraPanMsg(angle));
-//	}
-	
-	/**
-	 * Tilts the camera up and down.
-	 * 
-	 * @param angle The tilt angle.  The valid range is from
-	 * -20 to 30.  Zero degrees is straight forward. Negative is down, positive is up.  
-	 * @return whether the operation was successful.
-	 */
-//	public boolean tiltCamera(double angle) {
-//		
-//		// Some sanity checks...
-//		if (angle < -20 || angle > 30) {
-//			log("tileCamera: ERROR: Invalid camera tilt angle (" + angle + ")...");
-//			return false;
-//		}
-//		
-//		log("tiltCamera: Sending tilt camera command to DemoServer...");
-//		return sendMsg(new CameraTiltMsg(angle));
-//	}
-	
-	/**
-	 * Takes a snapshot using the camera.
-	 * 
-	 * @return The image taken, or null if error.
-	 */
-	public BufferedImage takeSnapshot() {
-		Logger.log("Sending take camera snapshot command to DemoServer...");
-		
-		// Create the message
-		CameraTakeSnapshotMsg takeSnapshotMsg = new CameraTakeSnapshotMsg();
-		
-		// Set the reply address and port so an acknowledgment can be returned.
-		takeSnapshotMsg.setReplyAddr(localAddress);
-		takeSnapshotMsg.setPort(localPort);
-		
-		// Send the message
-		try {
-			rcvMsg = null;
-			tcpSender.sendMessage(destAddr, destPort, takeSnapshotMsg);
-		} catch (PharosException e1) {
-			e1.printStackTrace();
-			Logger.logErr("ERROR: Failed to send message.");
-			return null;
-		}
-		
-		BufferedImage result = null;
-		
-		// Wait until an ack arrives...
-		synchronized(this) {
-			if (rcvMsg == null) {
-				Logger.log("Waiting for CameraSnapshotMsg...");
-				try {
-					//wait(MAX_ACK_LATENCY);
-					
-					// TODO: abort this wait if network connectivity to robot is broken.
-					wait();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-			
-			if (rcvMsg != null) {
-				if (rcvMsg instanceof CameraSnapshotMsg) {
-					CameraSnapshotMsg ackMsg = (CameraSnapshotMsg)rcvMsg;
-					Logger.log("CameraSnapshotMsg received, success = " + ackMsg.getSuccess());
-					if (ackMsg.getSuccess()) {
-						try {
-							result = ackMsg.getImage();
-						} catch(IOException e) {
-							e.printStackTrace();
-							Logger.logErr("Failed to get image from CameraSnapshotMsg, error = " + e.getMessage());
-						}
-					} else
-						Logger.logErr("CameraSnapshotMsg.getSuccess() returned false.");
-				} else
-					Logger.logErr("Ack received but is of wrong type: " + rcvMsg);
-				rcvMsg = null;
-			} else
-				Logger.logErr("Ack not received...");
-		}
-		
-		return result;
-	}
-	
-//	private void logErr(String msg) {
-//		String result = "CmdExec: " + msg;
-//		System.err.println(result);
-//		if (flogger != null)
-//			flogger.log(result);
-//	}
-//	
-//	private void log(String msg) {
-//		String result = "CmdExec: " + msg;
-//		if (System.getProperty ("PharosMiddleware.debug") != null)
-//			System.out.println(result);
-//		if (flogger != null)
-//			flogger.log(result);
-//	}
 }
