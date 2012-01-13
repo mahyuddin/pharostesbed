11c6ff2377b5be3ea8dbde6b98672b220422c2dd
kchang-pharos <kchang-pharos@53a56b47-fd32-4597-bdd3-0ed485b86959>
2012-01-13 21:46:50 +0000

    Commented out output statements and fixed indentation.

--- PharosMiddleware/src/pharoslabut/demo/simonsays/scratch/ScratchMessage.java
+++ PharosMiddleware/src/pharoslabut/demo/simonsays/scratch/ScratchMessage.java
@@ -7,58 +7,57 @@
    The two types:
       broadcast "<name-string>"
       sensor-update <name-string> <value-string>
-  
+
    We only support one name-value pair in a sensor-update message,
    and we assume the value is a string.
 
    The quotes are removed from name-string, and the "Scratch-" prefix
    is deleted (if present).
-*/
+ */
 package pharoslabut.demo.simonsays.scratch;
 
 public class ScratchMessage
 {
-  // possible message types
-  public static final int BROADCAST_MSG = 0;
-  public static final int SENSOR_UPDATE_MSG = 1;
-  public static final int UNKNOWN_MSG = 2;
+    // possible message types
+    public static final int BROADCAST_MSG = 0;
+    public static final int SENSOR_UPDATE_MSG = 1;
+    public static final int UNKNOWN_MSG = 2;
 
-  private int msgType = UNKNOWN_MSG;   // default values
-  private String msgTypeStr = "unknown";
-  private String varName = null;
-  private String valStr = null;  
+    private int msgType = UNKNOWN_MSG;   // default values
+    private String msgTypeStr = "unknown";
+    private String varName = null;
+    private String valStr = null;  
+
+    public ScratchMessage(int type, String typeStr, String name, String val) {
+        msgType = type;
+        msgTypeStr = typeStr;
+        varName = name;
+        valStr = val;
+    }
+
+    // accessor methods
+
+    public int getMessageType()
+    {  return msgType;  }
+
+    public String getMessageTypeStr()
+    {  return msgTypeStr;  }
+
+    public String getName()
+    {  return varName;  }
+
+    public String getValue()
+    {  return valStr;  }
 
 
-  public ScratchMessage(int type, String typeStr, String name, String val) {
-      msgType = type;
-      msgTypeStr = typeStr;
-      varName = name;
-      valStr = val;
-  }
-  
-  // accessor methods
-
-  public int getMessageType()
-  {  return msgType;  }
-
-  public String getMessageTypeStr()
-  {  return msgTypeStr;  }
-
-  public String getName()
-  {  return varName;  }
-
-  public String getValue()
-  {  return valStr;  }
-
-
-  public String toString()
-  {
-    if (msgType == BROADCAST_MSG)
-      return msgTypeStr + " " + valStr;
-    else if (msgType ==  SENSOR_UPDATE_MSG)
-      return msgTypeStr + " " + varName + " " + valStr;
-    else // unknown message
-      return msgTypeStr;  
-  }  // end of toString()
+    public String toString()
+    {
+        if (msgType == BROADCAST_MSG)
+            return msgTypeStr + " " + valStr;
+        else if (msgType ==  SENSOR_UPDATE_MSG)
+            return msgTypeStr + " " + varName + " " + valStr;
+        else // unknown message
+            return msgTypeStr;  
+    }  // end of toString()
 
 }  // end of ScratchMessage class
