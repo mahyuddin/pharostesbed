3e819acc24b626254cf7b1a2052dbe740d3b15f9
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2012-02-09 15:27:31 +0000

    Debugged.  Tested to work

--- trunk/Proteus3/Traxxas/MCU/Driver_v1/SndTestCmdThreaded.py
+++ trunk/Proteus3/Traxxas/MCU/Driver_v1/SndTestCmdThreaded.py
@@ -11,29 +11,45 @@ Date: 02/08/2012
 '''
 
 import serial, sys, binascii, time, struct
+from threading import Thread
 
+'''
+SerialMonitor operates as a separate thread that 
+receives incoming data from the Traxxas.
+'''
 class SerialMonitor(Thread): # SerialMonitor extends Thread
-	def __init__(self, ser):
+
+	''' 
+	The constructor of SerialMonitor.
+
+	Parameters:
+	 - ser: The serial port object
+         - watchdog: A list object that when non-empty allows the
+           SerialMonitor's loop to exit.
+	'''
+	def __init__(self, ser, watchdog):
 		Thread.__init__(self) # Call superclass constructor
 		self.ser = ser;
+		self.watchdog = watchdog
 		
-		def run():
-			while True:
-				while (ser.inWaiting() >= 10):
-					sys.stdout.write("Receiving 10 bytes...\n")
-					rxdata = ser.read(10)
+	def run(self):
+		print "Serial Monitor thread starting."
+		while(len(self.watchdog) == 0):
+			while (ser.inWaiting() >= 10):
+				sys.stdout.write("Receiving 10 bytes...\n")
+				rxdata = ser.read(10)
 					
-					# Format chars: http://docs.python.org/library/struct.html#format-characters
-					resp = struct.unpack("<hhHhh", rxdata)
-					sys.stdout.write("target speed = " + str(resp[0]) \
-						+ ", current speed = " + str(resp[1]) \
-						+ ", motor cmd = " + str(resp[2]) \
-						+ ", prev err = " + str(resp[3]) \
-						+ ", total err = " + str(resp[4]) \
-						+ "\n")
-					#sys.stdout.write("Received " + str(struct.unpack("<hhHhh", rxdata)) + "\n\n")
-					#sys.stdout.write("Received " + str(struct.unpack("b"*10, rxdata)) + "\n\n")
-					sys.stdout.flush()
+				# Format chars: http://docs.python.org/library/struct.html#format-characters
+				resp = struct.unpack("<hhHhh", rxdata)
+				sys.stdout.write("target speed = " + str(resp[0]) \
+					+ ", current speed = " + str(resp[1]) \
+					+ ", motor cmd = " + str(resp[2]) \
+					+ ", prev err = " + str(resp[3]) \
+					+ ", total err = " + str(resp[4]) \
+					+ "\n")
+				#sys.stdout.write("Received " + str(struct.unpack("<hhHhh", rxdata)) + "\n\n")
+				#sys.stdout.write("Received " + str(struct.unpack("b"*10, rxdata)) + "\n\n")
+				sys.stdout.flush()
 
 ser = serial.Serial("/dev/ttyUSB0", 115200)
 
@@ -45,18 +61,27 @@ ser.flushInput()
 print("Waiting two seconds for Arduino to start...")
 time.sleep(2)
 
+watchdog = []
+
 # Create and start a SerialMonitor thread
-m = SerialMonitor(read)
+m = SerialMonitor(ser, watchdog)
 m.start()
 
-data = (0x24, 0, 0, 0x64, 0, 0x40) # Move forward at 100cm/s
+data = (0x24, 0, 100, 0x40) # Move forward at 100cm/s with 0 degree steering angle
+bytes = struct.pack("<BhhB", *data)
 
-bytes = struct.pack("BBBBBB", *data)
+#data = (0x24, 0, 0, 0x64, 0, 0x40) # Move forward at 100cm/s
+#bytes = struct.pack("BBBBBB", *data)
 
-# Transmit the message at 10Hz to prevent the safety
-# stop from being triggered.
-while True:
-	numTX = ser.write(bytes)
-	sys.stdout.write("Wrote " + str(data) + ", num bytes: " + str(numTX) + "\n")
-	sys.stdout.flush()
-	time.sleep(0.1)  # sleep for 0.1 seconds
+try:
+	# Transmit the message at 10Hz to prevent the safety
+	# stop from being triggered.
+	while True:
+		numTX = ser.write(bytes)
+		sys.stdout.write("Sent " + str(data) + ", num bytes: " + str(numTX) + "\n")
+		sys.stdout.flush()
+		time.sleep(0.1)  # sleep for 0.1 seconds
+except (KeyboardInterrupt, SystemExit):
+	print "Control-C detected, halting program"
+	watchdog.append("quit")  # allow Monitor thread to exit
+	sys.exit()
