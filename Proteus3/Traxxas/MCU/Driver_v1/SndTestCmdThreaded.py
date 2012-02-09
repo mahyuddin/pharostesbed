19c3f5c2eef4905d732786acf1fb1ac3ef2996ba
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2012-02-09 16:10:17 +0000

    Added start byte and checksum to response message.

--- Proteus3/Traxxas/MCU/Driver_v1/SndTestCmdThreaded.py
+++ Proteus3/Traxxas/MCU/Driver_v1/SndTestCmdThreaded.py
@@ -13,6 +13,8 @@ Date: 02/08/2012
 import serial, sys, binascii, time, struct
 from threading import Thread
 
+PROTEUS_START = 0x24
+
 '''
 SerialMonitor operates as a separate thread that 
 receives incoming data from the Traxxas.
@@ -35,21 +37,38 @@ class SerialMonitor(Thread): # SerialMonitor extends Thread
 	def run(self):
 		print "Serial Monitor thread starting."
 		while(len(self.watchdog) == 0):
-			while (ser.inWaiting() >= 10):
-				sys.stdout.write("Receiving 10 bytes...\n")
-				rxdata = ser.read(10)
+			while (ser.inWaiting() >= 12):
+				startByte = ser.read(1)
+				if (startByte == chr(PROTEUS_START)):
+					#sys.stdout.write("Receiving 11 bytes...\n")
+					rxdata = ser.read(11)
 					
-				# Format chars: http://docs.python.org/library/struct.html#format-characters
-				resp = struct.unpack("<hhHhh", rxdata)
-				sys.stdout.write("target speed = " + str(resp[0]) \
-					+ ", current speed = " + str(resp[1]) \
-					+ ", motor cmd = " + str(resp[2]) \
-					+ ", prev err = " + str(resp[3]) \
-					+ ", total err = " + str(resp[4]) \
-					+ "\n")
-				#sys.stdout.write("Received " + str(struct.unpack("<hhHhh", rxdata)) + "\n\n")
-				#sys.stdout.write("Received " + str(struct.unpack("b"*10, rxdata)) + "\n\n")
-				sys.stdout.flush()
+					# Format chars: http://docs.python.org/library/struct.html#format-characters
+					respBytes = struct.unpack("<" + "B"*11, rxdata)
+					#print "Received " + str(respBytes)
+					checksum = PROTEUS_START
+					for b in respBytes[:-1]:
+						checksum ^= b
+						#print "  after XORing " + str(b) + " checksum = " + str(checksum) + \	
+						#	" (0x" + binascii.b2a_hex(chr(checksum)) + ")"
+
+					resp = struct.unpack("<hhHhhB", rxdata)
+					if (chr(checksum) == chr(resp[5])):
+						sys.stdout.write("target speed = " + str(resp[0]) \
+							+ ", current speed = " + str(resp[1]) \
+							+ ", motor cmd = " + str(resp[2]) \
+							+ ", prev err = " + str(resp[3]) \
+							+ ", total err = " + str(resp[4]) \
+							+ "\n")
+						#sys.stdout.write("Received " + str(struct.unpack("<hhHhh", rxdata)) + "\n\n")
+						#sys.stdout.write("Received " + str(struct.unpack("b"*10, rxdata)) + "\n\n")
+						sys.stdout.flush()
+					else:
+						print "Checksum mismatch 0x" + binascii.b2a_hex(chr(checksum)) + " != 0x" \
+							+ binascii.b2a_hex(chr(resp[5]))
+				else:
+					print "Invalid start byte 0x" + binascii.b2a_hex(startByte) + " != 0x" + \
+						binascii.b2a_hex(chr(PROTEUS_START)) + ", discarding"
 
 ser = serial.Serial("/dev/ttyUSB0", 115200)
 
@@ -67,7 +86,7 @@ watchdog = []
 m = SerialMonitor(ser, watchdog)
 m.start()
 
-data = (0x24, 0, 100, 0x40) # Move forward at 100cm/s with 0 degree steering angle
+data = (PROTEUS_START, 0, 100, 0x40) # Move forward at 100cm/s with 0 degree steering angle
 bytes = struct.pack("<BhhB", *data)
 
 #data = (0x24, 0, 0, 0x64, 0, 0x40) # Move forward at 100cm/s
