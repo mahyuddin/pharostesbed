dfaa60b431fad39e85832b2d2075297af754198f
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2012-02-14 18:12:30 +0000

    modified to allow user to decide when to send the start byte to the motor controller

--- trunk/Proteus3/Traxxas/MCU/TestSyRenSWSerial/TestSyRenSWSerial.ino
+++ trunk/Proteus3/Traxxas/MCU/TestSyRenSWSerial/TestSyRenSWSerial.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial swSerial(10, 11);  // pin 10 = RX, pin 11 = TX
+SoftwareSerial swSerial(11, 10);  // RX, TX
 
 #define LED_PIN 13  // This is connected to the on-board LED
 
@@ -17,15 +17,11 @@ byte _checksum = 0;
 byte _ledState = 0;
 char _buff[10];
 int _indx = 0;
+boolean _init = false;
 
 void setup() {
   Serial.begin(115200);
-  
-  swSerial.begin(9600);
-  delay(3000); // allow motor controller to boot
-  
-  swSerial.write(START_BYTE); // set the baud rate
-  
+  swSerial.begin(2400);
   pinMode(LED_PIN, OUTPUT);  // enable LED
 }
 
@@ -33,41 +29,47 @@ void loop() {
   // Read in a speed command
   while (Serial.available() && _indx < 9) {
     _buff[_indx++] = Serial.read();
-    if (_buff[_indx-1] == 0x0A) {  // if last char is newline char
+    if (_buff[_indx-1] == 0x0A) {  // if last char is a newline char
       _indx = 0;
-      int speed = atoi(_buff);
-      
-      if (speed <= 127 && speed >= -127) {
-        if (speed > 0) {
-          _cmd = CMD_FORWARD;
-          _data = speed;
-        } else {
-          _cmd = CMD_BACKWARD;
-          _data = -speed;
-        }
-        _checksum = 0x7F & (SYREN_ADDR + _cmd + _data);
-      
-        Serial.print("Speed: ");
-        Serial.print(speed);
-        Serial.print(", Cmd: ");
-        Serial.print(_cmd);
-        Serial.print(", Data: ");
-        Serial.print(_data);
-        Serial.print(", Checksum: ");
-        Serial.println(_checksum);
+      if (_buff[0] == 'i') {
+        swSerial.write(START_BYTE); // set the baud rate
+        _init = true;
+        Serial.println("Motor Controller Initialized.");
       } else {
-        Serial.println("Invalid speed.");
+        int speed = atoi(_buff);
+      
+        if (speed <= 127 && speed >= -127) {
+          if (speed > 0) {
+            _cmd = CMD_FORWARD;
+            _data = speed;
+          } else {
+            _cmd = CMD_BACKWARD;
+            _data = -speed;
+          }
+          _checksum = 0x7F & (SYREN_ADDR + _cmd + _data);
+      
+          Serial.print("Speed: ");
+          Serial.print(speed);
+          Serial.print(", Cmd: ");
+          Serial.print(_cmd);
+          Serial.print(", Data: ");
+          Serial.print(_data);
+          Serial.print(", Checksum: ");
+          Serial.println(_checksum);
+        } else {
+          Serial.println("Invalid speed.");
+        }
+        digitalWrite(LED_PIN, _ledState);
+        _ledState = !_ledState;
       }
-    
-      digitalWrite(LED_PIN, _ledState);
-      _ledState = !_ledState; 
     }
   }
 
-  swSerial.write(SYREN_ADDR); // send address byte
-  swSerial.write(_cmd);
-  swSerial.write(_data);
-  swSerial.write(_checksum);
-  
+  if (_init) {
+    swSerial.write(SYREN_ADDR); // send address byte
+    swSerial.write(_cmd);
+    swSerial.write(_data);
+    swSerial.write(_checksum);
+  }
   delay(100);
 }
