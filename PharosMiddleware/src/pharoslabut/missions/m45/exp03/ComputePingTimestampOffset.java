8622d214dd13c5b401e8b7e7169b6cd7d9151012
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2012-05-25 02:23:51 +0000

    Added javadoc.

--- PharosMiddleware/src/pharoslabut/missions/m45/exp03/ComputePingTimestampOffset.java
+++ PharosMiddleware/src/pharoslabut/missions/m45/exp03/ComputePingTimestampOffset.java
@@ -4,6 +4,15 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
+/**
+ * This program computes the offset in milliseconds between the ping timestamp 
+ * and the java timstamp.  
+ * 
+ * When the ping was performed on April 25, 2012, the offset is 1335312000000 ms.
+ * When the pint was performed on May 24, 2012, the offset is 1337817600000 ms.
+ * 
+ * @author Chien-Liang Fok
+ */
 public class ComputePingTimestampOffset {
 
 	public ComputePingTimestampOffset() {
@@ -18,7 +27,8 @@ public class ComputePingTimestampOffset {
 		cal.set(1970, GregorianCalendar.JANUARY, 1, 0, 0);
 		Date date1 = cal.getTime();
 		
-		cal.set(2012, GregorianCalendar.APRIL, 25, 0, 0);
+		//cal.set(2012, GregorianCalendar.APRIL, 25, 0, 0);
+		cal.set(2012, GregorianCalendar.MAY, 24, 0, 0);
 		Date date2 = cal.getTime();
 		
 		//long startDate = cal.getTime();
