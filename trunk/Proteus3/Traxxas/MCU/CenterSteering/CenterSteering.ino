af82a5f8dd04c31bcc368b49feba67eb39301ab6
chien-liang <chien-liang@53a56b47-fd32-4597-bdd3-0ed485b86959>
2012-09-19 14:58:25 +0000

    Initial commit

---
+++ trunk/Proteus3/Traxxas/MCU/CenterSteering/CenterSteering.ino
@@ -0,0 +1,23 @@
+/**
+ * Centers the front wheels of the Traxxas Stampede chassis.
+ *
+ * @author Chien-Liang Fok
+ */
+#include <Servo.h>
+
+#define LEFT 20
+#define CENTER 100
+#define RIGHT 180
+#define PAUSE 15
+#define MIN_PULSE 1000
+#define MAX_PULSE 2000
+Servo servo;
+int pos;
+
+void setup() {
+  servo.attach(9, MIN_PULSE, MAX_PULSE);
+  servo.write(CENTER);
+}
+
+void loop() {
+}
